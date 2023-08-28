163ae9a45daca20854643060d4d28137afef21ef
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-08-28 20:17:21 +0530

    Time: 15 ms (21.96%), Space: 8.9 MB (65.65%) - LeetHub

---
+++ 2819-remove-trailing-zeros-from-a-string/2819-remove-trailing-zeros-from-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+        while(num.size() > 0 && num.back() == '0'){
+            num.pop_back();
+        }
+        return num;
+    }
+};
