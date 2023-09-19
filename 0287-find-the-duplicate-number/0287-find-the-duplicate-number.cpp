f3bf114142ebe546d4cb3e5d9772c9068af4859d
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-09-19 08:12:00 +0530

    Time: 319 ms (5.01%), Space: 103.7 MB (6.89%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int sz = nums.size();
+        map<int, int> mp;
+        
+        for(auto i : nums)
+            mp[i]++;
+
+        for(auto i : mp)
+            if(i.second > 1) return i.first;
+
+        return -1;
+    }
+};
