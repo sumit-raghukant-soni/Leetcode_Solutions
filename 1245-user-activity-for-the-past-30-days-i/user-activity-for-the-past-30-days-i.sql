c4c643afdcb48cd7a31baa95669c60f700b9d0c7
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-11-16 18:30:35 +0530

    Time: 765 ms (86.15%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,2 +1,2 @@
 
-SELECT activity_date as day, COUNT(DISTINCT user_id) as active_users FROM activity GROUP BY activity_date HAVING activity_date BETWEEN "2019-06-28" AND "2019-07-27";
+SELECT activity_date as day, COUNT(DISTINCT user_id) as active_users FROM (SELECT * FROM activity WHERE activity_date BETWEEN "2019-06-28" AND "2019-07-27") tab GROUP BY activity_date;
