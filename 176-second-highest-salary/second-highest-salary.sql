f92e2ad4ce970c5e2e4f8b6db05a456c7da0393c
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-11-18 11:00:29 +0530

    Time: 515 ms (32.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,6 @@
+SELECT IFNULL(
+                (
+                    SELECT salary FROM employee GROUP BY salary ORDER BY salary DESC
+                 LIMIT 1 OFFSET 1)
+                , NULL) 
+AS SecondHighestSalary;
