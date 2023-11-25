7d2084d5522bff6f54a45c8e255d0e08ff255d0a
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-11-25 23:16:52 +0530

    Time: 4 ms (47.93%) | Memory: 7.9 MB (15.04%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool check(string& tmp, string& str1, string& str2){
+        int sz1 = tmp.length(), sz2 = str1.length(), sz3 = str2.length();
+        if(sz2%sz1 != 0 || sz3%sz1 != 0) return false;
+
+        int i=0;
+        while(i <sz2){
+            if(str1.substr(i, sz1) != tmp) return false;
+            i += sz1;
+        }
+        i=0;
+        while(i <sz3){
+            if(str2.substr(i, sz1) != tmp) return false;
+            i += sz1;
+        }
+        return true;
+    }
+    string gcdOfStrings(string str1, string str2) {
+        int sz1 = str1.length(), sz2 = str2.length();
+
+        string tmp = "", ans = "";
+        int j=0;
+        while(j<min(sz1, sz2)){
+            if(str1[j] == str2[j]) {
+                tmp += str1[j++];
+                bool c = check(tmp, str1, str2);
+                if(c) ans = tmp;
+            }
+            else break;
+        }
+
+        return ans;
+    }
+};
