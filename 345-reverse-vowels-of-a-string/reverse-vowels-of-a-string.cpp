ad9908859237ceab4ba77d32e3527ddd42c12855
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-11-25 23:56:17 +0530

    Time: 10 ms (29.43%) | Memory: 8.4 MB (20.26%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -6,11 +6,15 @@ public:
         int i=0, j=sz-1;
         
         while(i < j){
-            while(i<j && (ans[i] != 'a' && ans[i] != 'e' && ans[i] != 'i' && ans[i] != 'o' && ans[i] != 'u') && (ans[i] != 'A' && ans[i] != 'E' && ans[i] != 'I' && ans[i] != 'O' && ans[i] != 'U')){
+            char ch = ans[i];
+            while(i<j && (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') && (ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U')){
                 i++;
+                ch = ans[i];
             }
-            while(j>i && (ans[j] != 'a' && ans[j] != 'e' && ans[j] != 'i' && ans[j] != 'o' && ans[j] != 'u') && (ans[j] != 'A' && ans[j] != 'E' && ans[j] != 'I' && ans[j] != 'O' && ans[j] != 'U')){
+            ch = ans[j];
+            while(j>i && (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') && (ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U')){
                 j--;
+                ch = ans[j];
             }
             if(i < j) swap(ans[i++], ans[j--]);
         }
