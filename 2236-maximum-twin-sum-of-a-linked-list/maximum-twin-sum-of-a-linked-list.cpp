55560d0783c3803fa89e83fdf26b6aaea44056d7
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-12-02 15:10:30 +0530

    Time: 245 ms (7.06%) | Memory: 149.5 MB (5.05%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int pairSum(ListNode* head) {
         ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
-       map<int, int> mp;
+       unordered_map<int, int> mp;
        int sz = 0, cnt = 0;
        ListNode* curr = head;
        while(curr){
@@ -10,18 +10,17 @@ public:
            sz++;
        } 
        curr = head;
+       int maxi = INT_MIN;
        while(curr){
            if(cnt < sz/2)
                mp[cnt] = curr->val;
-           else
+           else{
                mp[(sz/2) - (cnt - (sz/2) + 1)] += curr->val;
+               maxi = max(maxi, mp[(sz/2) - (cnt - (sz/2) + 1)]);
+           }
            curr = curr->next;
            cnt++;
        }
-        int maxi = INT_MIN;
-
-        for(auto i : mp) maxi = max(maxi, i.second);
-
         return maxi;
     }
 };
