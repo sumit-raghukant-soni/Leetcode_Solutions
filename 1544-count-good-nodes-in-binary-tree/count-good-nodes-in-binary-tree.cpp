dc898e3b6e191315dc30c370be1ac92b686c1dea
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-12-22 17:41:13 +0530

    Time: 111 ms (27.82%) | Memory: 86.6 MB (47.51%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void solve(TreeNode* root, int& ans, int maxi){
+        if(root == NULL) return;
+
+        if(maxi <= root->val) ans++;
+        solve(root->left, ans, max(maxi, root->val));
+        solve(root->right, ans, max(maxi, root->val));
+    }
+    int goodNodes(TreeNode* root) {
+        if(root == NULL) return {};
+
+        int ans = 0;
+        solve(root, ans, root->val);
+
+        return ans;        
+    }
+};
