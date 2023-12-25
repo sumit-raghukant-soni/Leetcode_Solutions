f2b7539761479e0df078660e95446b2071c8b8de
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-12-26 00:43:22 +0530

    Time: 20 ms (74.40%) | Memory: 16.2 MB (55.11%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    void bfs(int ind, unordered_map<int, vector<int>>& adj, unordered_map<int, int>& vis){
+    void bfs(int ind, int sz, vector<vector<int>>& adj, unordered_map<int, int>& vis){
         if(vis[ind]) return;
         vis[ind]++;
 
-        for(int i=0; i<adj[ind].size(); i++){
-            if(!vis[adj[ind][i]]) bfs(adj[ind][i], adj, vis);
+        for(int i=0; i<sz; i++){
+            if(ind != i && adj[ind][i] && !vis[i]) bfs(i, sz, adj, vis);
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int cnt = 0, sz = isConnected.size();
-        unordered_map<int, vector<int>> adj;
         unordered_map<int, int> vis;
-        for(int i=0; i<sz; i++){
-            for(int j=0; j<sz; j++){
-                if(isConnected[i][j] && j != i) adj[i].push_back(j);
-            }
-        }
 
         cnt++;
-        bfs(0, adj, vis);
+        bfs(0, sz, isConnected, vis);
 
         for(int i=0; i<sz; i++){
             if(!vis[i]){
                 cnt++;
-                bfs(i, adj, vis);
+                bfs(i, sz, isConnected, vis);
             }
         }
 
