2ab8e39b9d2da42baa0fe7c3394927152512d35c
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-12-26 13:35:25 +0530

    Time: 18 ms (85.87%) | Memory: 8.7 MB (65.29%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int MOD = 1000000007;
-    int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n+1, vector<int>(target+1));
-        dp[0][0] = 1;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=target; j++){
-                for(int l=1; l<=j && l <= k; l++){
-                    dp[i][j] = (dp[i][j] + dp[i-1][j - l]) % MOD;
-                }
-            }
+    int solve(vector<vector<int>>& dp, int n, int target, int k){
+        if(n == 0 && target == 0) return 1;
+        if(n <= 0 || target <= 0) return 0;
+
+        if(dp[n][target] != -1) return dp[n][target] % MOD;
+
+        int ways = 0;
+        for(int i=1; i<=k; i++){
+            ways = (ways + solve(dp, n-1, target - i, k)) % MOD;
         }
+        return dp[n][target] = ways;
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+
+        solve(dp, n, target, k);
 
         return dp[n][target];
     }
