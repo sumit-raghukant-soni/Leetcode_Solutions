dc1a2607cffef37bdbc30e366fae41962d7015d0
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-12-26 14:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (73.77%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
+    int BS(vector<int>& nums, int& sz, int s, int e){
+        if(s > e) return 0;
+
+        int mid = s + (e-s)/2;
+
+        int prev = mid-1 >= 0 ? nums[mid-1] : INT_MIN;
+        int next = mid+1 < sz ? nums[mid+1] : INT_MIN;
+
+        if(prev < nums[mid] && next < nums[mid]) return mid;
+
+        int left = BS(nums, sz, s, mid-1);
+        int right = BS(nums, sz, mid+1, e);
+
+        return left ? left : right;
+    }
     int findPeakElement(vector<int>& nums) {
         int sz = nums.size();
-
-        for(int i=0; i<sz; i++){
-            int prev = i-1 >= 0 ? nums[i-1] : INT_MIN;
-            int next = i+1 < sz ? nums[i+1] : INT_MIN;
-            if(nums[i] > prev && nums[i] > next) return i;
-        }
-
-        return 0;
+        return BS(nums, sz, 0, sz-1);
     }
 };
