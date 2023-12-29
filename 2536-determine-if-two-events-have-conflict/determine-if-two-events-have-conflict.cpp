a6eca516cb7e94aa269771c27d4bcee486b21a90
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-12-30 02:10:09 +0530

    Time: 7 ms (12.66%) | Memory: 11.8 MB (90.26%) - LeetSync

--- 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.cpp
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
-    bool solve(vector<string>& event1, vector<string>& event2){
-        string sh1 = event1[0].substr(0, 2), sh2 = event2[0].substr(0, 2);
-        string sm1 = event1[0].substr(3, 2), sm2 = event2[0].substr(3, 2);
-        string eh1 = event1[1].substr(0, 2), eh2 = event2[1].substr(0, 2);
-        string em1 = event1[1].substr(3, 2), em2 = event2[1].substr(3, 2);
-
-        if(sh1 == eh1){
-            if(eh1 == sh2 && sm1 <= sm2 && sm2 <= em1) return true;
-        }
-        else if(sh1 < sh2 && sh2 < eh1){
-            return true;
-        }
-        else if(sh1 <= sh2 && sh2 < eh1){
-            return sm1 <= sm2;
-        }
-        else if(sh1 < sh2 && sh2 <= eh1){
-            return sm2 <= em1;
-        }
-
-        return false;
-    }
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        return solve(event1, event2) || solve(event2, event1);
+        int s1 = stoi(event1[0].substr(0,2)) * 60 + stoi(event1[0].substr(3,2));
+        int e1 = stoi(event1[1].substr(0,2)) * 60 + stoi(event1[1].substr(3,2));
+        int s2 = stoi(event2[0].substr(0,2)) * 60 + stoi(event2[0].substr(3,2));
+        int e2 = stoi(event2[1].substr(0,2)) * 60 + stoi(event2[1].substr(3,2));
+        
+        return (s1 <= s2 && s2 <= e1) || (s2 <= s1 && s1 <= e2);
     }
 };
