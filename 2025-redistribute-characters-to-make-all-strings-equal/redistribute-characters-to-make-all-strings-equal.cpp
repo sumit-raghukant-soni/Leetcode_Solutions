0551f5623ec2e2bc863474f87e5e270c6a58405b
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2023-12-30 09:11:41 +0530

    Time: 26 ms (6.23%) | Memory: 12.7 MB (21.01%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+    int alpha[26];
     bool makeEqual(vector<string>& words) {
-        int alpha[26] = {0};
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int sz = words.size();
 
         for(auto i : words)
             for(char j : i) alpha[j - 'a']++;
 
-        for(auto i : alpha) if(i%sz != 0) return false;
+        for(int i=0; i<26; i++) if(alpha[i]%sz != 0) return false;
 
         return true;
     }
