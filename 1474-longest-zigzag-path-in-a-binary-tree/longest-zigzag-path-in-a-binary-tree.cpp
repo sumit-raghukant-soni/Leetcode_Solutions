ca1d7d5bfc2d10e394fd01596ada7c13360302cd
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-04 10:50:39 +0530

    Time: 144 ms (29.75%) | Memory: 95.2 MB (28.54%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(TreeNode* root, int& ans, string dir, int tmp){
+    int solve(TreeNode* root, int& ans, int dir, int tmp){
         if(!root) return 0;
 
         if(!root->left && !root->right){
@@ -9,20 +9,19 @@ public:
         }
 
         int left = 0, right = 0;
-        if(dir == ""){
-            left = solve(root->left, ans, "right", 1);
-            right = solve(root->right, ans, "left", 1);
+        if(dir == -1){
+            left = solve(root->left, ans, 1, 1);
+            right = solve(root->right, ans, 0, 1);
         }
-        else if(dir == "left"){
-            left = solve(root->left, ans, "right", tmp + 1);
-            right = solve(root->right, ans, "left", 1);
+        else if(dir == 0){
+            left = solve(root->left, ans, 1, tmp + 1);
+            right = solve(root->right, ans, 0, 1);
         }
         else{
-            left = solve(root->left, ans, "right", 1);
-            right = solve(root->right, ans, "left", tmp + 1);
+            left = solve(root->left, ans, 1, 1);
+            right = solve(root->right, ans, 0, tmp + 1);
         }
 
-        cout << root->val << dir << " " << tmp << " " << ans << endl;
         ans = max(ans, tmp);
         return 1 + max(left, right);
     }
@@ -30,7 +29,7 @@ public:
         if(!root) return 0;
         
         int ans = 0;
-        solve(root, ans, "", 0);
+        solve(root, ans, -1, 0);
 
         return ans;
     }
