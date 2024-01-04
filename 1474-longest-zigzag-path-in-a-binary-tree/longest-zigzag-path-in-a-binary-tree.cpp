ce73f2d04a1594544a682c8957860dafd5aaabf1
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-04 11:31:11 +0530

    Time: 131 ms (56.07%) | Memory: 95 MB (36.17%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    int solve(TreeNode* root, int& ans, int dir, int tmp){
-        if(!root) return 0;
-
-        if(!root->left && !root->right){
-            ans = max(tmp, ans);
-            return 1;
-        }
-
-        int left = 0, right = 0;
-        if(dir == -1){
-            left = solve(root->left, ans, 1, 1);
-            right = solve(root->right, ans, 0, 1);
-        }
-        else if(dir == 0){
-            left = solve(root->left, ans, 1, tmp + 1);
-            right = solve(root->right, ans, 0, 1);
-        }
-        else{
-            left = solve(root->left, ans, 1, 1);
-            right = solve(root->right, ans, 0, tmp + 1);
-        }
-
+    void solve(TreeNode* root, int& ans, int dir, int tmp){
+        if(!root) return;
         ans = max(ans, tmp);
-        return 1 + max(left, right);
+        
+        if(!root->left && !root->right) return;
+
+        solve(root->left, ans, 1, dir ? 1 : tmp + 1);
+        solve(root->right, ans, 0, dir ? tmp + 1 : 1);
     }
     int longestZigZag(TreeNode* root) {
         if(!root) return 0;
         
         int ans = 0;
-        solve(root, ans, -1, 0);
+        solve(root, ans, 0, 0);
 
         return ans;
     }
