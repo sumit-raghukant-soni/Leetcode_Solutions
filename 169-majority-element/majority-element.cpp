417d6fbc3061dd06993444e60ce72947737e31ac
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-13 11:02:52 +0530

    Time: 17 ms (37.75%) | Memory: 20 MB (34.63%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        int sz = nums.size();
+    // O(N) Space-Complexity Soln
+    // int majorityElement(vector<int>& nums) {
+    //     unordered_map<int, int> mp;
+    //     int sz = nums.size();
 
-        for(int i : nums){
-            mp[i]++;
-            if(mp[i] > sz/2) return i;
+    //     for(int i : nums){
+    //         mp[i]++;
+    //         if(mp[i] > sz/2) return i;
+    //     }
+
+    //     return -1;
+    // }
+
+    // OR O(1) Space-Complexity Soln
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sz = nums.size(), cnt;
+
+        for(int i = 0; i<sz; i++){
+            cnt = 1;
+            while(i+1 < sz && nums[i] == nums[i+1]){
+                i++, cnt++;
+            }
+            if(cnt > sz/2) return nums[i];
         }
 
         return -1;
