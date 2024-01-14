9fe12eaf059638e2e99670c295c34d1516424aff
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-14 12:46:06 +0530

    Time: 69 ms (86.05%) | Memory: 20.8 MB (89.19%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        int sz1 = word1.length(), sz2 = word2.length();
-        map<char, int> mp1, mp2;
-        for(int i=0; i<sz1; i++)
-            mp1[word1[i]]++;    
+        int sz = word1.length();
+        if(word2.length() != sz) return false;
+        vector<int> alpha1(26, 0), alpha2(26, 0);
+        
+        for(int i=0; i<sz; i++){
+            alpha1[word1[i] - 'a']++;
+            alpha2[word2[i] - 'a']++;
+        }
 
-        for(int j=0; j<sz2; j++)
-            mp2[word2[j]]++;
+        for(int i=0; i<26; i++){
+            if((alpha1[i] == 0 && alpha2[i] != 0) || (alpha2[i] == 0 && alpha1[i] != 0)) return false;
+        }
 
-        if(mp1.size() != mp2.size()) return false;
-        for(auto i : mp1)
-            if(mp2.find(i.first) == mp2.end()) return false;
-
-        map<int, int> mp3, mp4;
-        for(auto i : mp1) mp3[i.second]++;
-        for(auto i : mp2) mp4[i.second]++;
-
-        return mp3 == mp4;
+        sort(alpha1.begin(), alpha1.end());
+        sort(alpha2.begin(), alpha2.end());
+        
+        return alpha1 == alpha2;
     }
 };
