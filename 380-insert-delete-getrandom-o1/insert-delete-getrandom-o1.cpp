5d548b61caa94337ca59bbe14345eef679c25e32
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-14 13:13:36 +0530

    Time: 192 ms (33.52%) | Memory: 97.4 MB (73.46%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,28 @@
+class RandomizedSet {
+public:
+    unordered_map<int, int> mp;
+    unordered_map<int, int>::iterator itr;
+    RandomizedSet() {
+        itr = mp.begin();
+    }
+    
+    bool insert(int val) {
+        if(mp.find(val) == mp.end()){
+            mp[val]++;
+            return true;
+        }
+        return false;
+    }
+    
+    bool remove(int val) {
+        if(mp.find(val) == mp.end()) return false;
+        mp.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        unordered_map<int, int>::iterator i = mp.begin();
+        i = next(i, rand()%mp.size());
+        return i->first;
+    }
+};
