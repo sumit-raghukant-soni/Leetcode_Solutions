8ad5df39288e2f8d5897e99dfbc7a29727e62f9b
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-15 14:44:56 +0530

    Time: 23 ms (30.16%) | Memory: 25.4 MB (25.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int sz = nums.size();
-        long long pro = 1;
-        int cnt = 0;
-        vector<int> ans(sz, 0);
+        vector<int> pre(sz, 0), suf(sz, 0), ans(sz, 0);
 
-        for(int i=0; i<sz; i++){
-            if(nums[i] != 0) pro *= nums[i];
-            if(nums[i] == 0) cnt++;
+        pre[0] = nums[0];
+        suf[sz-1] = nums[sz-1];
+
+        for(int i=1; i<sz; i++){
+            pre[i] = pre[i-1] * nums[i];
+            suf[sz-i-1] = suf[sz-i] * nums[sz-i-1];
         }
 
         for(int i=0; i<sz; i++){
-            if(cnt == 0 || (nums[i] == 0 && cnt == 1)){
-                if(nums[i] != 0) ans[i] = pro/nums[i];
-                else ans[i] = pro;
-            }
+            if(i == 0) ans[i] = suf[i+1];
+            else if(i == sz-1) ans[i] = pre[i-1];
+            else ans[i] = pre[i-1] * suf[i+1];
         }
 
         return ans;
