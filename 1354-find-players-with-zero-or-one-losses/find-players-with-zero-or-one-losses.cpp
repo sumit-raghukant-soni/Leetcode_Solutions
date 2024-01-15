cdbf849672f23c96187758aa6d39dc562413a391
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-15 14:08:02 +0530

    Time: 1324 ms (5.02%) | Memory: 157.9 MB (98.33%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -3,17 +3,17 @@ public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         int sz = matches.size();
         map<int, int> win, loss;
-        for(int i=0; i<sz; i++){
-            win[matches[i][0]]++;
+        for(int i=0; i<sz; i++)
             loss[matches[i][1]]++;
-        }
+        
         vector<int> a, b;
-        for(auto i : loss){
+        for(auto i : loss)
             if(i.second == 1) b.push_back(i.first);
-        }
-        for(auto i : win){
-            if(loss.find(i.first) == loss.end()) a.push_back(i.first);
-        }
+        
+        for(int i=0; i<sz; i++)
+            if(loss.find(matches[i][0]) == loss.end() && find(a.begin(), a.end(), matches[i][0]) == a.end()) a.push_back(matches[i][0]);
+        
+        sort(a.begin(), a.end());
         return {a, b};
     }
 };
