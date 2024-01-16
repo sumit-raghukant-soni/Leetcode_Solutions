07f413da53036d51b7c093b51a253c19c77aca0c
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-16 17:25:57 +0530

    Time: 15 ms (33.97%) | Memory: 20.5 MB (38.93%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,51 +2,24 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int cnt = 0, sz = height.size();
-        // vector<int> dp(sz, 0);
-        vector<int> left(sz, 0), right(sz, 0);
-        // int j=0;
-        // for(int i=0; i<sz; i++){
-        //     j=i+1;
-        //     int tmp = 0;
-        //     while(j<sz && height[j] < height[i]){
-        //         if(j == sz) break;
-        //         tmp = max(tmp, height[j++]);
-        //     }
-        //     // cout << i << " " << j << endl;
-        //     if(j != i+1){
-        //         if(j < sz) tmp = height[i];
-        //         int k = i+1;
-        //         while(k < j && k < sz-1){
-        //             dp[k] = (tmp - height[k]);
-        //             cnt += (tmp - height[k]);
-        //             k++;
-        //         }
-        //         i = k-1;
-        //     }
-        // }
+        vector<int> capacity(sz, 0);
+
         int tmp = 0;
         for(int i=0; i<sz; i++){
-            left[i] = tmp;
+            capacity[i] = tmp;
             tmp = max(tmp, height[i]);
         }
         tmp = 0;
         for(int i=sz-1; i>=0; i--){
-            right[i] = tmp;
+            capacity[i] = min(tmp, capacity[i]);
             tmp = max(tmp, height[i]);
         }
 
-        // for(int i : left) cout << i << " ";
-        //     cout << endl;
-        // for(int i : right) cout << i << " ";
-        //     cout << endl;
         for(int i=0; i<sz; i++){
-            if(min(left[i], right[i]) > height[i]){
-                // cout << min(left[i], right[i]) - height[i] << " ";
-                cnt += min(left[i], right[i]) - height[i];
+            if(capacity[i] > height[i]){
+                cnt += capacity[i] - height[i];
             }
         }
-        // for(int i : dp) cout << i << " ";
-        // cout << endl;
 
         return cnt;
     }
