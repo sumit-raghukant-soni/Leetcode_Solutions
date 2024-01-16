c6cfa0fcb1ae01204bd84eedea02025fe63266fc
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-16 18:11:21 +0530

    Time: 203 ms (26.97%) | Memory: 97.3 MB (73.46%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -2,27 +2,35 @@ class RandomizedSet {
 public:
     unordered_map<int, int> mp;
     unordered_map<int, int>::iterator itr;
+    int sz;
     RandomizedSet() {
-        itr = mp.begin();
+        sz = 0;
     }
     
     bool insert(int val) {
-        if(mp.find(val) == mp.end()){
-            mp[val]++;
-            return true;
-        }
-        return false;
+        if(mp.find(val) != mp.end()) return false;
+        mp[val]++;
+        sz++;
+        return true;
     }
     
     bool remove(int val) {
         if(mp.find(val) == mp.end()) return false;
         mp.erase(val);
+        sz--;
         return true;
     }
     
     int getRandom() {
-        itr = mp.begin();
-        itr = next(itr, rand()%mp.size());
+        itr = next(mp.begin(), rand()%sz);
         return itr->first;
     }
 };
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
