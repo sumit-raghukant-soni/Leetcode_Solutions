feef63f4e53bec717572b2ca67bd382118c73a2f
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-17 11:05:07 +0530

    Time: 2 ms (65.84%) | Memory: 8.6 MB (69.36%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -3,8 +3,9 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int, int> mp, mp2;
         
-        for(int i : arr)
-            mp[i]++;
+        int sz = arr.size();
+        for(int i =0; i<sz; i++)
+            mp[arr[i]]++;
 
         for(auto i : mp)
             mp2[i.second]++;
