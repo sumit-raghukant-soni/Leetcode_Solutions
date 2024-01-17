29cae9e1359b98f6b98a34a02597122ac24ac6d6
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-17 11:36:45 +0530

    Time: 5 ms (65.63%) | Memory: 6.3 MB (95.52%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string> thousand = {"", "M", "MM", "MMM"};
-        vector<string> hundred = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        vector<string> tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        vector<string> ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        string thousand[] = {"", "M", "MM", "MMM"};
+        string hundred[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
         return thousand[num/1000] + hundred[(num%1000)/100] + tens[(num%100)/10] + ones[(num%10)];
     }
