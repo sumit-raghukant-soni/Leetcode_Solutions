b28b3ae94404f5d40c67cb4fa230f183b43e259f
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-01-31 23:17:44 +0530

    Time: 6 ms (86.29%) | Memory: 15.6 MB (5.54%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -2,6 +2,7 @@ using func = function<int(int, int)>;
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
+        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
         int sz = tokens.size();
         stack<int> st;
         unordered_map<string, func> mp = {
