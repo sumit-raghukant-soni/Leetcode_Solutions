6be9b776d36b218071a9782ee8ee740dbfd9642e
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-02-08 00:48:53 +0530

    Time: 35 ms (13.98%) | Memory: 287.1 MB (5.02%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int solve(string s, int& i, int& sz){
-        // cout << endl << "ind " << s[i] << endl;
         int ans = 0, sign = 1, val = 0;
         for(; i<sz && s[i] != ')'; i++){
-            // cout << s[i] << ":" << val << "=" << ans << " " << sign << endl;
             if(s[i] >= '0' && s[i] <= '9') val = (val*10) + int(s[i] - '0');
             else if(s[i] == '-'){
                 ans += val * sign;
@@ -18,27 +16,20 @@ public:
             }
             else if(s[i] == '('){
                 i++;
-                int tmp = solve(s, i, sz);
-                // cout << "value gained " << tmp << endl;
-                val = tmp;
+                val = solve(s, i, sz);
             } 
-            // cout << ans << " ";
         }
-        // cout << "end value " << val * sign << " ";
         ans += val * sign;
-        // i++;
         return ans;
     }
     int calculate(string s) {
         string corrected = "";
         int sz = s.length(), ind = 1;
 
-        for(int i=0; i<sz; i++){
+        for(int i=0; i<sz; i++)
             if(s[i] != ' ') corrected += s[i];
-        }
         
         corrected = '(' + corrected + ')';
-        // cout << corrected << endl;
         sz++;
         return solve(corrected, ind, sz);
     }
