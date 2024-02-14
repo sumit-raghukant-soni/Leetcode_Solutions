bcd46d5714a0613ecdd266c731eb731292fe503e
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-02-14 21:22:47 +0530

    Time: 149 ms (69.54%) | Memory: 126.4 MB (42.43%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int sz = nums.size();
-        vector<int> ans(sz, 0);
-        int even = 0, odd = 1;
+        vector<int> ans(sz);
+        int even=0, odd=1;
+
         for(int i=0; i<sz; i++){
-            if(nums[i] > 0){
-                ans[even] = nums[i];
-                even+=2;
-            }
-            else{
+            if(nums[i] < 0){
                 ans[odd] = nums[i];
                 odd+=2;
+            }
+            else{
+                ans[even] = nums[i];
+                even+=2;
             }
         }
 
