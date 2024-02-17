fc60b9491cf2ed565a596e5a077fae1aad12b4c9
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-02-17 23:03:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (19.45%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -4,7 +4,13 @@ public:
         if(x == 0) return 0;
         int ans = 1;
 
-        for(int i=2; (1LL*i*i) <= x; i++) ans = i;
+        int s = 1, e = x/2, mid;
+        while(s <= e){
+            mid = s + (e-s)/2;
+            if(1LL*mid*mid <= x) ans = mid;
+            if(1LL*mid*mid > x) e = mid-1;
+            else s = mid+1;
+        }
 
         return ans;
     }
