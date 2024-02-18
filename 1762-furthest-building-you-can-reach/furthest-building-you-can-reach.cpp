67e37bf9bf453a69db6bd58d08741db170cc7eb8
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-02-18 12:36:58 +0530

    Time: 99 ms (18.27%) | Memory: 57.4 MB (30.85%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        int sz = heights.size(), i=0, diff;
+        priority_queue<int> q;
+
+        for(; i<sz-1; i++){
+            diff = heights[i+1] - heights[i];
+            if(diff <= 0) continue;
+
+            bricks -= diff;
+            q.push(diff);
+
+            if(bricks < 0){
+                cout << "pop " << q.top() << endl;
+                bricks += q.top();
+                q.pop();
+                ladders--;
+            }
+
+            if(ladders < 0) break;
+        }
+
+        return i;
+    }
+};
