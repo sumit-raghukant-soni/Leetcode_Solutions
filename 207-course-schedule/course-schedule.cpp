fd4a3a7280a137e3bb374f79b2901145df820c88
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-02-28 22:00:19 +0530

    Time: 15 ms (77.85%) | Memory: 17.5 MB (35.16%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        queue<int> q;
+        unordered_map<int, vector<int>> mp;
+        vector<int> indegree(numCourses, 0);
+        int sz = prerequisites.size();
+        if(sz == 0) return true;
+
+        for(int i=0; i<sz; i++){
+            mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            indegree[prerequisites[i][1]]++;
+        }
+
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        vector<int> topo;
+
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            cout << front << " ";
+            topo.push_back(front);
+            for(auto i : mp[front]){
+                indegree[i]--;
+                if(indegree[i] == 0) q.push(i);
+            }
+        }
+
+        return topo.size() == numCourses;
+    }
+};
