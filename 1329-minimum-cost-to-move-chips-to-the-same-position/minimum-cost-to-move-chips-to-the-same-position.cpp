477ab14aa02127d6a12cb03ea056af4073bab621
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-03-03 23:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (61.69%) - LeetSync

--- 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.cpp
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int minCostToMoveChips(vector<int>& position) {
-        int ans = INT_MAX, tmp;
-        int sz = position.size();
+        int sz = position.size(), evenPlace = 0, oddPlace = 0;
 
         for(int i=0; i<sz; i++){
-            int ideal = position[i];
-            tmp = 0;
-            for(int j=0; j<sz; j++){
-                int mini = min(position[i], position[j]), maxi = max(position[i], position[j]);
-                tmp += (maxi-mini)%2;
-            }
-            
-            ans = min(tmp, ans);
+            if(position[i]&1) oddPlace++;
+            else evenPlace++;
         }
-        return ans;
+
+        return oddPlace < evenPlace ? oddPlace : evenPlace;
     }
 };
