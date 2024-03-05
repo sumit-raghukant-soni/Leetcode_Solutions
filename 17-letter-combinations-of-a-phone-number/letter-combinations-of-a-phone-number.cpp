65f2290aa1840aa3be228ccb274dc93454c92268
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-03-05 23:58:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (12.10%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    void solve(vector<string>& tmp, int ind, string str, vector<string>& ans){
-        if(tmp.size() == ind){
-            ans.push_back(str);
-            return;
+    vector<string> solve(vector<string> vec1, vector<string> vec2){
+        int sz1 = vec1.size(), sz2 = vec2.size();
+        vector<string> ans;
+        for(int i=0; i<sz1; i++){
+            for(int j=0; j<sz2; j++){
+                ans.push_back(vec1[i] + vec2[j]);
+            }
         }
-        for(int i=0; i<tmp[ind].length(); i++){
-            str += tmp[ind][i];
-            solve(tmp, ind+1, str, ans);
-            str.pop_back();
-        }
+        return ans;
     }
     vector<string> letterCombinations(string digits) {
-        if(digits.size() == 0) return {};
-        vector<string> ans;
-        
-        unordered_map<char, string> mp = {{'2',"abc"}, {'3',"def"}, {'4',"ghi"}, {'5',"jkl"}, {'6',"mno"}, {'7',"pqrs"}, {'8',"tuv"}, {'9',"wxyz"}}; 
         int sz = digits.size();
         vector<string> tmp;
+        unordered_map<char, vector<string>> mp = {{'2',{"a","b","c"}}, {'3',{"d","e","f"}}, {'4',{"g","h","i"}}, {'5',{"j","k","l"}}, {'6',{"m","n","o"}}, {'7',{"p","q","r","s"}}, {'8',{"t","u","v"}}, {'9',{"w","x","y","z"}} }; 
+
         for(int i=0; i<sz; i++){
-            tmp.push_back(mp[digits[i]]);
+            if(i == 0) tmp = mp[digits[i]];
+            else {
+                tmp = solve(tmp, mp[digits[i]]);
+            }
         }
 
-        solve(tmp, 0, "", ans);
-
-        return ans;
+        return tmp;
     }
 };
