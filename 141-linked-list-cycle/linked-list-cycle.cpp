b230eea7f9dcccca3674439ee297f22a4e845ba2
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-03-07 21:42:19 +0530

    Time: 9 ms (47.64%) | Memory: 10.5 MB (54.74%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*, int> mp;
-        while(head != NULL){
-            if(mp.find(head) != mp.end()) return true;
-            mp[head]++;
-            head = head->next;
-        } 
+        if(!head || !head->next) return false;
+
+        ListNode *fast = head->next, *slow = head;        
+
+        while(fast && slow){
+            if(fast == slow) return true;
+            fast = fast->next;
+            slow = slow->next;
+            if(fast) fast = fast->next;
+        }
+
         return false;
     }
 };
