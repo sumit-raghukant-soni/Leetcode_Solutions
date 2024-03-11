dfe8a02cc7f8cba07517bcb85da80968ecdecff4
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-03-12 00:10:24 +0530

    Time: 3 ms (93.39%) | Memory: 11.9 MB (63.27%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        if(word1 == word2) return 0;
-        int lcsSz = 0;
         int sz1 = word1.length(), sz2 = word2.length();
-        if(sz1 == 0 || sz2 == 0) return max(sz1, sz2);
         vector<vector<int>> dp(sz1+1, vector<int>(sz2+1, 0));
 
         for(int i=1; i<=sz1; i++) dp[i][0] = i;
@@ -13,17 +10,10 @@ public:
         for(int i=1; i<=sz1; i++){
             for(int j=1; j<=sz2; j++){
                 if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
+                else dp[i][j] = min({dp[i-1][j], dp[i-1][j-1], dp[i][j-1]}) + 1;
             }
         }
-
-        // for(auto i : dp){
-        //     for(auto j : i){
-        //         cout << j << " ";
-        //     }
-        //     cout << endl;
-        // }
-
+        
         return dp[sz1][sz2];
     }
 };
