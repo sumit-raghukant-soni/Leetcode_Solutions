6635aba8dec8e0a321bb4fe268814f2e5b8f6fae
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-04-03 20:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (44.68%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     void solve(vector<int> nums, int& sz, vector<vector<int>>& ans, int ind){
-        if(ind == sz) return;
+        if(ind == sz-1){
+            ans.push_back(nums);
+            return;
+        }
         
         for(int i=ind; i<sz; i++){
             swap(nums[ind], nums[i]);
-            if(ind != i) ans.push_back(nums);
             solve(nums, sz, ans, ind+1);
             swap(nums[ind], nums[i]);
         }
@@ -14,7 +16,6 @@ public:
         vector<vector<int>> ans;
         int sz = nums.size();
         
-        ans.push_back(nums);
         solve(nums, sz, ans, 0);            
 
         return ans;
