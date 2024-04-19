9956cf51f982f6c7809cdab3b0884bbad2fb5e96
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-04-19 07:53:57 +0530

    Time: 4 ms (31.91%) | Memory: 9.8 MB (39.05%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,31 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> inorder;
-        TreeNode* cur = root;
+        
+        TreeNode* curr = root;
 
-        while (cur != NULL) {
-            if (cur->left == NULL) {
-                inorder.push_back(cur->val);
-                cur = cur->right;
-            } 
-            else {
-                TreeNode* prev = cur->left;
-                while (prev->right && prev->right != cur) {
+        while(curr){
+            if(!curr->left){
+                inorder.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr){
                     prev = prev->right;
                 }
 
-                if (prev->right == NULL) {
-                    prev->right = cur;
-                    cur = cur->left;
-                } 
-                else {
+                if(!prev->right){
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else{
                     prev->right = NULL;
-                    inorder.push_back(cur->val);
-                    cur = cur->right;
+                    inorder.push_back(curr->val);
+                    curr = curr->right;
                 }
             }
         }
+
         return inorder;
     }
 };
