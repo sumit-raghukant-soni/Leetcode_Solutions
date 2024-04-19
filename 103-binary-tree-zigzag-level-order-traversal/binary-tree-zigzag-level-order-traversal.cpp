a1b373806dfcbc2318c6b0cf61a846a843e4e10c
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-04-19 08:29:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (8.64%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,27 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         if(!root) return {};
-        vector<vector<int>> ans(1, vector<int>());
+        vector<vector<int>> ans;
+        vector<int> tmp;
+
         queue<TreeNode*> q;
         q.push(root);
         q.push(NULL);
-
         TreeNode* front = NULL;
-        int ind = 0;
+
+        bool dir = 1;
+
         while(!q.empty()){
             front = q.front();
             q.pop();
-            if(front == NULL){
+            
+            if(!front){
+                ans.push_back(tmp);
+                tmp.clear();
                 if(q.empty()) break;
-                front = q.front();
-                q.push(NULL);
-                q.pop();
-                ans.push_back({});
-                ind++;
+                else{
+                    front = q.front();
+                    q.pop();
+                    q.push(NULL);
+                    dir = !dir;
+                }
             }
-            if(ind&1) ans[ind].insert(ans[ind].begin(), front->val);
-            else ans[ind].push_back(front->val);
+            
+            if(dir) tmp.push_back(front->val);
+            else tmp.insert(tmp.begin(), front->val);
 
             if(front->left) q.push(front->left);
             if(front->right) q.push(front->right);
