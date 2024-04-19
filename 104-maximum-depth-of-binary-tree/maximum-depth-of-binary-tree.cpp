09c1f2b31a8659a452fd21078bb054d56eb6f5ea
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-04-19 08:37:09 +0530

    Time: 6 ms (61.88%) | Memory: 17.4 MB (80.23%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -1,9 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == NULL) return 0;
-        if(root->left == NULL && root->right == NULL) return 1;
+        if(!root) return 0;
 
-        return max(maxDepth(root->right), maxDepth(root->left)) + 1;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
