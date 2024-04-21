46ffc4df69d586a182c3bda6b72a5fe2ff91d0a1
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-04-21 17:53:07 +0530

    Time: 8 ms (54.55%) | Memory: 20.2 MB (60.79%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,18 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool solve(TreeNode* root, long long mini, long long maxi){
-        if(!root) return true;
-        // cout << "values " << mini << " " << root->val << " " << maxi << endl;
-        if(root->val <= mini || root->val >= maxi) return false;
-        bool left = solve(root->left, mini, root->val), right = solve(root->right, root->val, maxi);
-        // cout << root->val << " " << left << " " << right << endl;
-        return left && right;
+    bool checkBST(TreeNode* root, long mini, long maxi) {
+        if (!root) return true;
+
+        bool isValid = root->val > mini && root->val < maxi;
+        
+        if(!isValid) return false;
+        return checkBST(root->left, mini, root->val) && checkBST(root->right, root->val, maxi);
     }
-    bool isValidBST(TreeNode* root) {
-        if(!root) return false;
-
-        long long mini = 1LL*INT_MIN - 1, maxi = 1LL*INT_MAX + 1;
-
-        return solve(root, mini, maxi);        
+    bool isValidBST(TreeNode* root) { 
+        return checkBST(root, 1LL*INT_MIN-1, 1LL*INT_MAX+1); 
     }
 };
