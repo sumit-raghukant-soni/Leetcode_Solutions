b37603fec4a5585fbce0bba189d7d6a7a175f733
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-04-23 07:44:30 +0530

    Time: 11 ms (69.20%) | Memory: 25.4 MB (82.12%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,22 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int s, int e, int& ind){
+    TreeNode* solve(vector<int>& postorder, vector<int>& inorder, int& sz, int& ind, int s, int e){
         if(ind < 0 || s > e) return NULL;
 
-        TreeNode* tmp = new TreeNode(postorder[ind]);
+        int mid = find(inorder.begin(), inorder.end(), postorder[ind]) - inorder.begin();
+
+        TreeNode* root = new TreeNode(postorder[ind]);
         ind--;
-        int mid = find(inorder.begin(), inorder.end(), postorder[ind+1]) - inorder.begin();
-        tmp->right = solve(inorder, postorder, mid+1, e, ind);
-        tmp->left = solve(inorder, postorder, s, mid-1, ind);
-
-        return tmp;
-    }
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int sz = inorder.size(), ind = sz-1;
-        TreeNode* root = NULL;
-
-        root = solve(inorder, postorder, 0, sz-1, ind);
+        root->right = solve(postorder, inorder, sz, ind, mid+1, e);
+        root->left = solve(postorder, inorder, sz, ind, s, mid-1);
 
         return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int sz = postorder.size(), ind = sz-1;
+        return solve(postorder, inorder, sz, ind, 0, sz-1);
     }
 };
