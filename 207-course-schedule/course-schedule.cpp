cafbff22084914e1d8bad5e7815023c7f129a54a
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-05-01 07:35:02 +0530

    Time: 32 ms (6.73%) | Memory: 18.4 MB (24.68%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        queue<int> q;
         unordered_map<int, vector<int>> mp;
-        vector<int> indegree(numCourses, 0);
-        int sz = prerequisites.size();
-        if(sz == 0) return true;
-
-        for(int i=0; i<sz; i++){
-            mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
-            indegree[prerequisites[i][1]]++;
+        queue<int> q;
+        vector<int> indegree(numCourses, 0), vis(numCourses, 0);
+        for(vector<int> i : prerequisites){
+            mp[i[0]].push_back(i[1]);
+            indegree[i[1]]++;
         }
 
         for(int i=0; i<numCourses; i++){
-            if(indegree[i] == 0) q.push(i);
-        }
-
-        vector<int> topo;
-
-        while(!q.empty()){
-            int front = q.front();
-            q.pop();
-            // cout << front << " ";
-            topo.push_back(front);
-            for(auto i : mp[front]){
-                indegree[i]--;
-                if(indegree[i] == 0) q.push(i);
+            if(indegree[i] == 0){
+                q.push(i); vis[i]++;
             }
         }
 
-        return topo.size() == numCourses;
+        int courses = 0;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            courses++;
+            for(auto i : mp[front]){
+                cout << front << ":" << i << " ";
+                if(vis[i] == 0){
+                    indegree[i]--;
+                    if(indegree[i] == 0) {q.push(i); vis[i]++;}
+                }
+                else return false;
+            }
+        }
+
+        return courses == numCourses;
     }
 };
