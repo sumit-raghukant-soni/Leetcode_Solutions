f06b2d8750f2cda5f4b5d19c53730b6331fbd1e2
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-05-02 02:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (53.66%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
+        if(word1 == word2) return 0;
         int sz1 = word1.length(), sz2 = word2.length();
         vector<vector<int>> dp(sz1+1, vector<int>(sz2+1, 0));
 
-        for(int i=1; i<=sz1; i++) dp[i][0] = i;
-        for(int i=1; i<=sz2; i++) dp[0][i] = i;
+        for(int i=0; i<=sz1; i++) dp[i][0] = i;
+        for(int i=0; i<=sz2; i++) dp[0][i] = i;
 
         for(int i=1; i<=sz1; i++){
             for(int j=1; j<=sz2; j++){
-                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = min({dp[i-1][j], dp[i-1][j-1], dp[i][j-1]}) + 1;
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
             }
         }
-        
+
+        // for(auto i : dp){
+        //     for(int j : i) cout << j << " ";
+        //     cout << endl;
+        // }
+
         return dp[sz1][sz2];
     }
 };
