79f7eec9062661e38c1c642c599fe5556da273f7
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-05-07 19:15:12 +0530

    Time: 2 ms (66.94%) | Memory: 7.5 MB (63.02%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
@@ -7,7 +7,6 @@ public:
             for(int j=0; j<=limit; j++){
                 if(n-(i+j) < 0) break;
                 if(n-(i+j) <= limit){
-                    cout << i << " " << j << " " << n - (i+j) << endl;
                     ans++;
                 }
             }
