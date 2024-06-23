4021684faacc46b3de5ca94a9fcb855674e8573b
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-06-23 22:13:29 +0530

    Time: 138 ms (24.19%) | Memory: 87.2 MB (30.97%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,48 +1,55 @@
 class Solution {
 public:
-    vector<int> findPreSmallerElement(vector<int>& heights, int& sz){
-        vector<int> arr(sz, 0);
+    vector<int> nextSmaller(vector<int>& vec, int sz){
         stack<int> st;
         st.push(-1);
 
-        for(int i=0; i<sz; i++){
-            while(st.top() != -1 && heights[st.top()] >= heights[i]){
-                st.pop();
-            }
-            arr[i] = st.top();
-            st.push(i);
-        }
-
-        return arr;
-    }
-    vector<int> findNextSmallerElement(vector<int>& heights, int& sz){
-        vector<int> arr(sz, 0);
-        stack<int> st;
-        st.push(-1);
+        vector<int> ans(sz, -1);
 
         for(int i=sz-1; i>=0; i--){
-            while(st.top() != -1 && heights[st.top()] >= heights[i]){
-                st.pop();
-            }
-            arr[i] = st.top();
+            while(st.top() != -1 && vec[st.top()] >= vec[i]) st.pop();
+            
+            ans[i] = st.top();
+            
             st.push(i);
-        }
-
-        return arr;
-    }
-    int largestRectangleArea(vector<int>& heights) {
-        int sz = heights.size(), ans = 0;
-
-        vector<int> pre = findPreSmallerElement(heights, sz), next = findNextSmallerElement(heights, sz);
-
-        for(int i=0; i<sz; i++){
-            int l = heights[i];
-            if(next[i] == -1) next[i] = sz;
-            int b = next[i] - pre[i] - 1;
-            // cout << l << " " << next[i] << " " << pre[i] << endl;
-            ans = max(ans, l*b);
         }
 
         return ans;
+    }
+    vector<int> previousSmaller(vector<int> vec, int sz){
+        stack<int> st;
+        st.push(-1);
+
+        vector<int> ans(sz, -1);
+
+        for(int i=0; i<sz; i++){
+            while(st.top() != -1 && vec[st.top()] >= vec[i]) st.pop();
+
+            ans[i] = st.top();
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    int largestArea(vector<int>& mat, vector<int>& pre, vector<int>& next, int sz){
+        int ans = 0;
+
+        for(int i=0; i<sz; i++){
+            int l = mat[i];
+            if(next[i] == -1) next[i] = sz;
+            int b = next[i] - pre[i] - 1;
+            
+            int newArea = l*b;
+            ans = max(ans,  newArea);
+        }
+
+        return ans;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int sz = heights.size();
+        vector<int> pre = previousSmaller(heights, sz), next = nextSmaller(heights, sz);
+        return largestArea(heights, pre, next, sz);
     }
 };
