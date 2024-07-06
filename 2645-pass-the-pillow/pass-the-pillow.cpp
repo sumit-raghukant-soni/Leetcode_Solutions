d6eeb28446b7812f263ca05d455b115fc111a5f6
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-07-06 07:56:53 +0530

    Time: 2 ms (54.14%) | Memory: 7.2 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        if(time+1 <= n) return time+1;
-
-        int iterations = time/(n-1);
-        time = time%(n-1);
-
-        return iterations&1 ? (n-time) : (time+1);
+        return (time/(n-1))&1 ? (n-(time%(n-1))) : ((time%(n-1))+1);
     }
 };
