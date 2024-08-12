3a9edbf8d0f5a038fa8af3ef3e4598434bdfa62e
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-08-12 10:35:58 +0530

    Time: 199 ms (5.18%) | Memory: 25 MB (5.72%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,41 +1,25 @@
 class KthLargest {
 public:
-    vector<int> maxList;
-    int sz, ind;
+    int k;
+    vector<int> nums;
     KthLargest(int k, vector<int>& nums) {
-        maxList = vector<int>(k, INT_MIN);
-        sz = k, ind = -1;
-
-        for(int i : nums){
-            add(i);
-        }
-        // print();
+        this->k = k, this->nums = nums;
+        sort(this->nums.begin(), this->nums.end());
     }
     
     int add(int val) {
-        if(ind == -1){
-            ind++;
-            maxList[ind] = val;
-            return val;
+        int j = nums.size()-1;
+        nums.push_back(INT_MAX);
+        while(j >= 0 && nums[j] > val){
+            nums[j+1] = nums[j];
+            j--;
         }
-        if(ind == sz-1 && val < maxList[ind]) return maxList[ind];
-        
-        if(ind < sz-1) ind++;
-        int tmp = ind-1;
-        while(tmp >= 0 && maxList[tmp] < val){
-            if(tmp+1 < sz) maxList[tmp+1] = maxList[tmp];
-            tmp--;
-        }
-        // cout << ind << ":" << tmp+1 << endl;
-        if(tmp+1 < sz) maxList[tmp+1] = val;
-        
-        // print();
-        return maxList[ind];
-    }
+        nums[j+1] = val;
 
-    void print(){
-        for(int i : maxList) cout << i << " ";
-        cout << endl;
+        // for(int i : nums) cout << i << " ";
+        // cout << endl;
+
+        return nums[nums.size()-k];
     }
 };
 
