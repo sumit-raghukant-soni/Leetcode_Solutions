2bf58aac132fcc7fec0d8d813bbc381cdbb0ef61
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-09-03 09:47:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (77.66%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-        int ans = 0;
         int sz = s.length();
+        int sum = 0, k2 = k;
         for(int i=0; i<sz; i++){
-            int tmp = s[i] - 'a' + 1;
-            if(tmp < 10) ans += tmp;
-            else ans += (tmp/10) + (tmp%10);
-        }
-
-        while(k > 1){
-            int tmp = ans;
-            ans = 0;
-            while(tmp > 0){
-                ans += tmp%10;
-                tmp /= 10;
+            int val = s[i] - 'a' + 1;
+            while(val){
+                sum += val%10;
+                val /= 10;
             }
-            k--;
+        }
+        k2--;
+
+        while(k2>0){
+            k2--;
+            int newSum = 0;
+            while(sum){
+                newSum += sum%10;
+                sum /= 10;
+            }
+            sum = newSum;
+            if(sum < 10) return sum;
         }
 
-        return ans;
+
+        return sum;
     }
 };
