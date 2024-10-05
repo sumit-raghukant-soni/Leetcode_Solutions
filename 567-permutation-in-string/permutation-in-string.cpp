6a0563db4e264f7ba6de3e2e766a95fdf745ebd9
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-10-05 23:21:09 +0530

    Time: 5 ms (70.09%) | Memory: 8.8 MB (80.36%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int sz1 = s1.length(), sz2 = s2.length();
-        vector<int> alpha1(26, 0), alpha2(26, 0);
+        int alpha1[26], alpha2[26], flg;
 
         for(char ch : s1) alpha1[ch-'a']++;
 
         for(int i=0; i<sz2; i++){
             alpha2[s2[i]-'a']++;
             if(i >= sz1) alpha2[s2[i-sz1] - 'a']--;
-            if(i >= sz1-1 && alpha1 == alpha2) return true;
+            if(i >= sz1-1){
+                flg = 0;
+                for(int j=0; j<26; j++){
+                    if(alpha1[j] == alpha2[j]) flg++;
+                }
+                if(flg == 26) return true;
+            } 
         }
 
         return false;
