729ef1897ca4e94e84a66314d40011870fe93350
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-11-26 22:54:23 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (33.82%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.java
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -1,20 +1,9 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
         int[] degree = new int[n];
-        List<List<Integer>> adjList = new ArrayList<>();
-        HashMap<Pair<Integer, Integer>, Integer> mp = new HashMap<>();
-
-        for(int i=0; i<n; i++) adjList.add(new ArrayList<>());
 
         for(int i=0; i<edges.length; i++){
-            adjList.get(edges[i][0]).add(edges[i][1]);
-        }
-
-        for(int i=0; i<n; i++){
-            List<Integer> curr = adjList.get(i);
-            for(int j=0; j<curr.size(); j++){
-                degree[curr.get(j)]++;
-            }
+            degree[edges[i][1]]++;
         }
 
         int cnt = 0, ans = 0;
