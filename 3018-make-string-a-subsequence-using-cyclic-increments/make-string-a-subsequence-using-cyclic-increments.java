fe98762301298a481711cfb6ddf00a351443dc07
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-12-04 22:04:28 +0530

    Time: 7 ms (43.44%) | Memory: 45.7 MB (23.03%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
@@ -2,11 +2,15 @@ class Solution {
     public boolean canMakeSubsequence(String str1, String str2) {
         int sz1 = str1.length(), sz2 = str2.length(), j = 0;
         if(sz2 > sz1) return false;
+
+        char[] next = new char[26];
+        for(int i=0; i<26; i++){
+            next[i] = (char) ((i + 1)%26 + 97);
+        }
         
         
         for(int i=0; i<sz1; i++){
-            // System.out.println(i + " " + j);
-            if(j < sz2 && (str1.charAt(i) == str2.charAt(j) || ((int) (str1.charAt(i)+1-97)%26 + 97) == (int) str2.charAt(j))){
+            if(j < sz2 && (str1.charAt(i) == str2.charAt(j) || next[(int) str1.charAt(i) - 97] == str2.charAt(j))){
                 j++;
             }
         }
