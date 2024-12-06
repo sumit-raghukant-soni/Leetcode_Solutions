8322dad0ada11fa75a424a71f94635a2e6650def
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-12-06 23:46:14 +0530

    Time: 48 ms (45.65%) | Memory: 45.7 MB (74.55%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,17 +1,15 @@
 class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
         int j=0, sz = banned.length, cnt = 0;
-        Set<Integer> st = new HashSet<>();
         
         Arrays.sort(banned);
         
         for(int i=1; i<=n; i++){
             if(i > maxSum) break;
             if(j < sz){
-                if(banned[j] != i && (maxSum-i) >= 0 && !st.contains(i)){
+                if(banned[j] != i && (maxSum-i) >= 0){
                     maxSum -= i;
                     cnt++;
-                    st.add(i);
                 }
                 while(j < sz && banned[j] == i) j++;
             }
