a39cf769f35e989c88e988989747855dea4e26a4
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-12-26 22:58:10 +0530

    Time: 131 ms (40.71%) | Memory: 45.3 MB (16.19%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,23 @@
 class Solution {
     private int count = 0;
     public int findTargetSumWays(int[] nums, int target) {
-        
-        solve(nums, target, 0);
+        int sz = nums.length;
+        Map<Pair<Integer, Integer>, Integer> dp = new HashMap<>();
 
-        return count;
+        return solve(dp, nums, sz, target, 0);
     }
-    private void solve(int[] nums, int target, int i){
-        if(i == nums.length){
-            if(target == 0) count++;
-            return;
+    private int solve(Map<Pair<Integer, Integer>, Integer> dp, int[] nums, int sz, int target, int i){
+        if(i == sz){
+            if(target == 0) return 1;
+            return 0;
         }
         
-        solve(nums, target+nums[i], i+1);
-        solve(nums, target-nums[i], i+1);
+        if(dp.containsKey(new Pair(i, target))) return dp.get(new Pair(i, target));
+        
+        int add = solve(dp, nums, sz, target+nums[i], i+1);
+        int minus = solve(dp, nums, sz, target-nums[i], i+1);
+        
+        dp.put(new Pair(i, target), add + minus);
+        return dp.get(new Pair(i, target));
     }   
 }
