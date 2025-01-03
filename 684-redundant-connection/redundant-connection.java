97fa5b3ac5ba87975436fee2f8fcfb0be0d76977
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-01-03 23:12:09 +0530

    Time: 11 ms (9.06%) | Memory: 45.3 MB (11.21%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int sz = edges.length;
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+
+        for(int i=0; i<sz; i++){
+            if(!adjList.containsKey(edges[i][0])) adjList.put(edges[i][0], new ArrayList<>());
+            if(!adjList.containsKey(edges[i][1])) adjList.put(edges[i][1], new ArrayList<>());
+            adjList.get(edges[i][0]).add(edges[i][1]);
+            adjList.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        for(int i=sz-1; i>=0; i--){
+            adjList.get(edges[i][0]).remove((Integer) edges[i][1]);
+            adjList.get(edges[i][1]).remove((Integer) edges[i][0]);
+            if(check(adjList)) return edges[i];
+            adjList.get(edges[i][0]).add(edges[i][1]);
+            adjList.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        return null;
+    }
+
+    public boolean check(Map<Integer, List<Integer>> adjList){
+        int n = adjList.size();
+        Set<Integer> st = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+
+        while(!q.isEmpty()){
+            Integer curr = q.poll();
+            st.add(curr);
+            for(int i : adjList.get(curr)){
+                if(!st.contains(i)) q.add(i);
+            }
+        }
+
+        return st.size() == n;
+    }
+}
