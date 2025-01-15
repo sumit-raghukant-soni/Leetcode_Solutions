86937a0b05d5a5d6d4f35a7271d19431824fcb16
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-01-15 19:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (63.21%) - LeetSync

--- 2509-minimize-xor/minimize-xor.java
+++ 2509-minimize-xor/minimize-xor.java
@@ -1,49 +1,28 @@
 class Solution {
     public int minimizeXor(int num1, int num2) {
-        int bits1[] = new int[31], bits2[] = new int[31];
-        int i=31, ans = 0, cnt1 = 0, cnt2 = 0;
+        int cnt1 = 0, cnt2 = 0, ans = 0, k, tmp = num2;
 
-        while(i > 0){
-            int rem1 = num1&1, rem2 = num2&1;
-            num1 = num1 >> 1;
-            num2 = num2 >> 1;
-            i--;
-            bits1[i] = rem1;
-            // bits2[i] = rem2;
-            cnt1 += rem1;
-            cnt2 += rem2;
+        while(tmp > 0){
+            if((tmp&1) == 1) cnt1++;
+            tmp = tmp >> 1;
         }
 
-        // i=30;
-        // for(; i>=0; i--){
-        //     System.out.print(bits1[i]);
-        // }
-        // System.out.println();
-        // i=30;
-        // for(; i>=0; i--){
-        //     System.out.print(bits2[i]);
-        // }
-        // System.out.println();
-
-        i = 0;
-        for(; i<31 && cnt2 > 0; i++){
-            if(bits1[i] == 1){
-                // System.out.println("Here " + i + " " + Math.pow(2, 30-i));
-                bits2[i] = 1; cnt2--; cnt1--;
-                ans += Math.pow(2, 30-i);
+        k=30;
+        while(cnt2 < cnt1 && k >= 0){
+            int val = (int) Math.pow(2, k--);
+            if((num1&val) >= 1){
+                ans += val;
+                cnt2++;
             }
         }
 
-        // System.out.println(ans + " " + cnt2);
-
-        i = 30;
-        for(; i>=0 && cnt2 > 0; i--){
-            // System.out.println(bits2[i]);
-            if(bits2[i] != 1 && cnt2 > 0){
-                bits2[i] = 1; cnt2--;
-                ans += Math.pow(2, 30-i);
+        k=0;
+        while(cnt2 < cnt1){
+            int val = (int) Math.pow(2, k++);
+            if((ans&val) == 0){
+                ans += val;
+                cnt2++;
             }
-            // System.out.println(ans + " " + cnt2);
         }
 
         return ans;
