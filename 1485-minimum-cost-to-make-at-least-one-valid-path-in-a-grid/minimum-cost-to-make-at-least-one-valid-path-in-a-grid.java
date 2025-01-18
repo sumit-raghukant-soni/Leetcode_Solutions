2513f8b427f3685d6f88c15733e0885ce3bf7633
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-01-18 11:13:03 +0530

    Time: 65 ms (5.11%) | Memory: 45.3 MB (34.31%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -12,22 +12,16 @@ class Solution {
             int i = curr.getKey(), j = curr.getValue();
             pq.poll();
             mp.put(curr, cost);
-            // System.out.println(i + ":" + j + " = " + cost + ", " + prei + ":" + prej);
+            // System.out.println(i + ":" + j + " = " + cost);
             if(i == m-1 && j == n-1) return cost;
 
             for(int k=0; k<4; k++){ 
-                int newi = i + dr[k], newj = j + dc[k];
+                int newi = i + dr[k], newj = j + dc[k], newcost = (grid[i][j] == (k+1) ? cost : cost+1);
                 Pair<Integer, Integer> newcurr = new Pair(newi, newj);
-                if(newi < m && newi >=0 && newj < n && newj >= 0){
+                if(newi < m && newi >=0 && newj < n && newj >= 0 && (!mp.containsKey(newcurr) || mp.get(newcurr) > newcost)){
                     // System.out.println(newi + ":" + newj + " " + mp.containsKey(newcurr));
-                    if(grid[i][j] == (k+1) && (!mp.containsKey(newcurr) || mp.get(newcurr) > cost)) {
-                        mp.put(newcurr, cost);
-                        pq.add(new Pair(cost, newcurr));
-                    }
-                    else if(!mp.containsKey(newcurr) || mp.get(newcurr) > (cost+1)){
-                        mp.put(newcurr, cost+1);
-                        pq.add(new Pair(cost+1, newcurr));
-                    }
+                    mp.put(newcurr, newcost);
+                    pq.add(new Pair(newcost, newcurr));
                 }
             }
         }
