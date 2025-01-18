16c56b969c3747ab954643e7a76b8c72cb53712a
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-01-18 10:52:58 +0530

    Time: 68 ms (5.11%) | Memory: 45.5 MB (25.55%) - LeetSync

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minCost(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        PriorityQueue<Pair<Integer, Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>>> pq = new PriorityQueue<>((a, b) -> a.getKey() - b.getKey());
+        pq.add(new Pair(0, new Pair(new Pair(0, 0), new Pair(-1, -1))));
+        Map<Pair<Integer, Integer>, Integer> mp = new HashMap<>();
+
+        int dr[] = {0, 0, 1, -1}, dc[] = {1, -1, 0, 0};
+        while(!pq.isEmpty()){
+            int cost = pq.peek().getKey();
+            Pair<Integer, Integer> curr = pq.peek().getValue().getKey(), prev = pq.peek().getValue().getValue();
+            int i = curr.getKey(), j = curr.getValue(), prei = prev.getKey(), prej = prev.getValue();
+            pq.poll();
+            mp.put(curr, cost);
+            // System.out.println(i + ":" + j + " = " + cost + ", " + prei + ":" + prej);
+            if(i == m-1 && j == n-1) return cost;
+
+            for(int k=0; k<4; k++){ 
+                int newi = i + dr[k], newj = j + dc[k];
+                Pair<Integer, Integer> newcurr = new Pair(newi, newj);
+                if(newi < m && newi >=0 && newj < n && newj >= 0 && (newi != prei || newj != prej)){
+                    // System.out.println(newi + ":" + newj + " " + mp.containsKey(newcurr));
+                    if(grid[i][j] == (k+1) && (!mp.containsKey(newcurr) || mp.get(newcurr) > cost)) {
+                        mp.put(newcurr, cost);
+                        pq.add(new Pair(cost, new Pair(newcurr, curr)));
+                    }
+                    else if(!mp.containsKey(newcurr) || mp.get(newcurr) > (cost+1)){
+                        mp.put(newcurr, cost+1);
+                        pq.add(new Pair(cost+1, new Pair(newcurr, curr)));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
