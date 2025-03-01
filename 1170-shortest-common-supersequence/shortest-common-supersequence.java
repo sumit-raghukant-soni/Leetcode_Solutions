74a02d30726ce5df0e3c5f0a49c05f393e2cda79
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-03-01 10:32:12 +0530

    Time: 31 ms (10.32%) | Memory: 47.7 MB (87.59%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,9 +1,6 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         int sz1 = str1.length(), sz2 = str2.length();
-        if(sz1 == 0) return str2;
-        if(sz2 == 0) return str1;
-        if(sz1 == 0 && sz2 == 0) return "";
         int dp[][] = new int[sz1][sz2];
 
         int a, b;
@@ -20,8 +17,6 @@ class Solution {
             }
         }
 
-        // System.out.println(dp[sz1-1][sz2-1]);
-
         String ans = "";
         int i = sz1-1, j = sz2-1;
         while(i-1 >= 0 && dp[i][j] == dp[i-1][j]){
@@ -29,13 +24,11 @@ class Solution {
             i--;
         }
 
-        String inter = "";
         int lasti = 0, lastj = 0;
         while(i >= 0 && j >= 0){
             if(j-1 >= 0 && dp[i][j] == dp[i][j-1]){
                 ans = str2.charAt(j) + ans;
                 j--;
-                // if(dp[i][j] == 0) ans = str1.charAt(i) + ans;
             }
             else if(i-1 >= 0 && dp[i-1][j] == dp[i][j]){
                 ans = str1.charAt(i) + ans;
@@ -43,14 +36,12 @@ class Solution {
             }
             else if(dp[i][j] == 0) break;
             else{
-                inter = str2.charAt(j) + inter;
                 ans = str2.charAt(j) + ans;
                 i--; j--;
             }
             lasti = i; lastj = j;
         }
 
-        // System.out.println(lasti + " " + lastj + " " + inter + " " + ans);
         for(i=lasti; i>=0; i--){
             if(dp[i][lastj >= 0 ? lastj : 0] == 0) ans = str1.charAt(i) + ans;
         }
@@ -58,13 +49,6 @@ class Solution {
         for(i=lastj; i>=0; i--){
             ans = str2.charAt(i) + ans;
         }
-
-        // for(i=0; i<sz1; i++){
-        //     for(j=0; j<sz2; j++){
-        //         System.out.print(dp[i][j] + ", ");
-        //     }
-        //     System.out.println();
-        // }
 
         return ans;
     }
