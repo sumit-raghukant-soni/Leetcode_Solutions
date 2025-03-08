9382f80a4dc0d5a10f0d01a44dde86350dbbabc3
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-03-08 11:13:42 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (28.51%) - LeetSync

--- 400-nth-digit/nth-digit.java
+++ 400-nth-digit/nth-digit.java
@@ -1,26 +1,18 @@
 class Solution {
     public int findNthDigit(int n) {
-        long num = 1, k = 1;
-        
-        while(n > 0){
-            if(n - k <= 0) break;
-            n -= k;
-            // if(n <= 100) 
-            // System.out.print(num + " " + n + " " + k + ", ");
-            if(num == 9 || num == 99 || num == 999 || num == 9999 || num == 99999 || num == 999999 || num == 9999999 || num == 99999999) k++;
-            num++;
+        long start = 1, end = 9, k = 1;
+
+        while(n > k*end){
+            n -= k*end;
+            end *= 10;
+            start *= 10;
+            k++;
         }
 
-        // System.out.println(num);
+        long num = start + (n-1)/k;
 
-        // n--;
-        while(n > 1){
-            n--;
-            // System.out.println(num);
-            num = num%((int) Math.pow(10, --k));
-        }
+        String ans = String.valueOf(num);
 
-        k--;
-        return (int) num/((int) Math.pow(10, k--));
+        return ans.charAt( (int)((n-1)%k) ) - '0';
     }
 }
