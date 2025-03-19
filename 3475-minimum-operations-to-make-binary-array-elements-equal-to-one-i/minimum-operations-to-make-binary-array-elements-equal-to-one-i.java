6d8ee1f82dc96946564c1b6114369d9db73e7cfd
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-03-19 21:56:31 +0530

    Time: 7 ms (66.53%) | Memory: 57.1 MB (84.19%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,23 +1,21 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int sz = nums.length, ans = 0, sum = 0;
+        int sz = nums.length, ans = 0, sum = 0, tmp = 0;
 
         for(int i=0; i<sz; i++) sum += nums[i];
 
         for(int i=0; i<sz-2; i++){
             if(nums[i] == 0){
                 ans++;
-                sum -= (nums[i]+nums[i+1]+nums[i+2]);
+                tmp = (nums[i]+nums[i+1]+nums[i+2]);
+                if(tmp == 2) sum -= 1;
+                else if(tmp == 1) sum += 1;
+                else sum += 3;
                 nums[i] = nums[i]^1;
                 nums[i+1] = nums[i+1]^1;
                 nums[i+2] = nums[i+2]^1;
-                sum += (nums[i]+nums[i+1]+nums[i+2]);
             }
         }
-
-        // for(int i=0; i<sz; i++){
-        //     System.out.print(nums[i] + ",");
-        // }
 
         return sum == sz ? ans : -1;
     }
