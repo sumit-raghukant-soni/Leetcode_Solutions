33fc77a4e2a5a080be88ada4cc8d0e8da7118d82
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-03-31 23:06:12 +0530

    Time: 23 ms (13.55%) | Memory: 52.2 MB (17.85%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -4,14 +4,14 @@ class Solution {
     public List<List<Integer>> findSubsequences(int[] nums) {
         List<Integer> tmp = new ArrayList<>();
 
-        solve(nums, tmp, 0);
+        solve(nums, tmp, -101, 0);
 
         ans.addAll(allSub);
         
         return ans;
     }
 
-    private void solve(int[] nums, List<Integer> tmp, int i){
+    private void solve(int[] nums, List<Integer> tmp, int last, int i){
         int sz = nums.length;
         if(tmp.size() >= 2){
             List<Integer> t = new ArrayList<>();
@@ -20,10 +20,10 @@ class Solution {
         }
         if(i == sz) return;
 
-        solve(nums, tmp, i+1);
-        if(tmp.isEmpty() || tmp.get(tmp.size()-1) <= nums[i]){
+        solve(nums, tmp, last, i+1);
+        if(last <= nums[i]){
             tmp.add(nums[i]);
-            solve(nums, tmp, i+1);
+            solve(nums, tmp, nums[i], i+1);
             tmp.remove(Integer.valueOf(nums[i]));
         }
     }
