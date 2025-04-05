7e039e89a2ec36195dec28049f69f8e7b5f051a5
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-04-05 17:48:03 +0530

    Time: 7 ms (6.83%) | Memory: 48.3 MB (5.12%) - LeetSync

---
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Map<Integer, List<TreeNode>> mp = new HashMap<>();
+    public List<TreeNode> allPossibleFBT(int n) {
+        
+        for(int i=0; i<=n; i++) mp.put(i, new ArrayList<>());
+        mp.get(0).add(null);
+        mp.get(1).add(new TreeNode(0));
+
+        for(int i=3; i<=n; i+=2){
+            createTree(i);
+        }
+
+        return mp.get(n);
+    }
+
+    private void createTree(int n){
+
+        TreeNode root = new TreeNode(0);
+
+        for(int i=1; i<n; i+=2){
+            int left = i;
+            int right = (n-1)-i;
+            for(TreeNode l : mp.get(left) ){
+                for(TreeNode r : mp.get(right) ){
+                    root.left = l;
+                    root.right = r;
+                    mp.get(n).add(clone(root));
+                }
+            }
+        }
+
+    }
+
+    private TreeNode clone(TreeNode root){
+        if(root == null) return null;
+        
+        TreeNode curr = new TreeNode(0);
+        if(root.left != null) curr.left = clone(root.left);
+        if(root.right != null) curr.right = clone(root.right);
+
+        return curr;
+    }
+}
