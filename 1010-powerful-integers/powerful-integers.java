6f7bdd3589cca9345ec91ef85d83f98930aab42e
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-04-10 20:59:26 +0530

    Time: 2 ms (30.98%) | Memory: 41.1 MB (85.33%) - LeetSync

--- 1010-powerful-integers/powerful-integers.java
+++ 1010-powerful-integers/powerful-integers.java
@@ -2,17 +2,17 @@ class Solution {
     public List<Integer> powerfulIntegers(int x, int y, int bound) {
         List<Integer> ans = new ArrayList<>();
         Set<Integer> st = new HashSet<>();
-        long a = 1, b = 1, bound2 = bound;
+        int a = 1, b = 1, bound2 = bound;
         
         for(long i=0; i<32; i++){
             b = 1;
-            if(a >= Integer.MAX_VALUE) break;
             for(long j=0; j<32; j++){
-                if((a+b) < Integer.MAX_VALUE && (a + b) <= bound2) st.add(Math.toIntExact(a+b));
+                if((1L*a+b) < Integer.MAX_VALUE && (a + b) <= bound2) st.add(a+b);
                 else break;
                 b *= y;
             }
-            a *= x;
+            if((1L * a * x) <= Integer.MAX_VALUE) a *= x;
+            else break;
         }
 
         ans.addAll(st);
