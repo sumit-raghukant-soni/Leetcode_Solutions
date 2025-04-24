5acc62b906a62e2c1ff1232dbf36d88a292c8be4
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-04-24 22:14:54 +0530

    Time: 67 ms (7.35%) | Memory: 45.6 MB (27.14%) - LeetSync

---
+++ 971-shortest-bridge/shortest-bridge.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int shortestBridge(int[][] grid) {
+        int n = grid.length, ans = Integer.MAX_VALUE;
+        PriorityQueue<Pair<Integer, Pair<Integer, Integer>>> q = new PriorityQueue<>((a, b) -> a.getKey() - b.getKey());
+        int vis[][] = new int[n][n];
+        int dr[] = {0, 1, 0, -1}, dc[] = {1, 0, -1, 0};
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    q.add(new Pair(0, new Pair(i, j)));
+                    break;
+                }
+            }
+            if(!q.isEmpty()) break;
+        }
+
+        while(!q.isEmpty()){
+            int val = q.peek().getKey();
+            int r = q.peek().getValue().getKey(), c = q.peek().getValue().getValue();
+            q.poll();
+            // System.out.println(r + ":" + c + " " + val);
+            if(grid[r][c] == 1 && val != 0) ans = Math.min(ans, val);
+            if(vis[r][c] == 1) continue;
+            vis[r][c] = 1;
+            for(int i=0; i<4; i++){
+                int newr = r + dr[i], newc = c + dc[i];
+                if(newr < n && newc < n && newr >= 0 && newc >= 0 && vis[newr][newc] == 0){
+                    // System.out.println(newr + " " + newc);
+                    if(grid[newr][newc] == 0) q.add(new Pair(val + 1, new Pair(newr, newc)));
+                    else q.add(new Pair(val, new Pair(newr, newc)));
+                }
+            }
+            // System.out.println(q);
+        }
+
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
