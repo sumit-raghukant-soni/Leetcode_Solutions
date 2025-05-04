90aad1db1497b219ab7f396a077f96e2823040fe
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-05-04 10:21:06 +0530

    Time: 4 ms (75.57%) | Memory: 52.8 MB (74.30%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
@@ -1,19 +1,19 @@
 class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
         int sz = dominoes.length, ans = 0;
-        Map<Pair<Integer, Integer>, Integer> mp = new HashMap<>();
+        int dp[][] = new int[10][10];
 
         for(int i=0; i<sz; i++){
-            int a = dominoes[i][0] > dominoes[i][1] ? dominoes[i][1] : dominoes[i][0];
-            int b = dominoes[i][0] < dominoes[i][1] ? dominoes[i][1] : dominoes[i][0];
-            Pair<Integer, Integer> p = new Pair(a,b);
-            mp.put(p, mp.getOrDefault(p, 0) + 1);
+            int a = Math.min(dominoes[i][0], dominoes[i][1]);
+            int b = Math.max(dominoes[i][0], dominoes[i][1]);
+            dp[a][b]++;
         }
 
-        // System.out.println(mp);
-        for(Map.Entry<Pair<Integer, Integer>, Integer> e : mp.entrySet()){
-            if(e.getValue() > 1){
-                ans += (e.getValue() * (e.getValue()-1))/2;
+        for(int i=1; i<10; i++){
+            for(int j=i; j<10; j++){
+                if(dp[i][j] > 1){
+                    ans += (dp[i][j] * (dp[i][j]-1)) / 2;
+                }
             }
         }
 
