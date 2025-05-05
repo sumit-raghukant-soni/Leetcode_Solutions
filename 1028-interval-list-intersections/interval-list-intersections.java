3139db0f695dbfcec9fcb83e383363ec1a3056ad
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-05-05 21:25:42 +0530

    Time: 28 ms (5.24%) | Memory: 45.9 MB (31.01%) - LeetSync

---
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        int sz1 = firstList.length, sz2 = secondList.length;
+        List<Pair<Integer, Integer>> pairs = new ArrayList<>();
+
+        int i=0, j=0;
+        while(i < sz1 && j < sz2){
+            // System.out.print(i + " " + j + ", ");
+            int s1 = firstList[i][0], e1 = firstList[i][1];
+            int s2 = secondList[j][0], e2 = secondList[j][1];
+
+            if((s2 <= e1 && e1 <= e2) || (s2 <= s1 && s1 <= e2) || (s2 <= s1 && e1 <= e2) || (s1 <= s2 && e2 <= e1)){
+                System.out.print("added");
+                pairs.add( new Pair( Math.max(s1, s2), Math.min(e1, e2) ) );
+                if(e2 > e1) i++;
+                else j++;
+            }
+            else if(e2 < e1) j++;
+            else i++;
+        }
+
+        int sz3 = pairs.size(), ans[][] = new int[sz3][2];
+        for(i=0; i<sz3; i++){
+            ans[i][0] = pairs.get(i).getKey();
+            ans[i][1] = pairs.get(i).getValue();
+        }
+
+        return ans;
+    }
+}
