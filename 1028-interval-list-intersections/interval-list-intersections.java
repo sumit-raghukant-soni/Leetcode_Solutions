b191ccb853adc1a46d7994f74386664db09310dc
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-05-05 21:26:02 +0530

    Time: 5 ms (17.52%) | Memory: 45.5 MB (82.66%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.java
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -5,12 +5,10 @@ class Solution {
 
         int i=0, j=0;
         while(i < sz1 && j < sz2){
-            // System.out.print(i + " " + j + ", ");
             int s1 = firstList[i][0], e1 = firstList[i][1];
             int s2 = secondList[j][0], e2 = secondList[j][1];
 
             if((s2 <= e1 && e1 <= e2) || (s2 <= s1 && s1 <= e2) || (s2 <= s1 && e1 <= e2) || (s1 <= s2 && e2 <= e1)){
-                System.out.print("added");
                 pairs.add( new Pair( Math.max(s1, s2), Math.min(e1, e2) ) );
                 if(e2 > e1) i++;
                 else j++;
