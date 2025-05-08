6f5fb542dbe488c6922ce0c2e7543da1c6042d48
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-05-08 22:04:37 +0530

    Time: 1205 ms (5.29%) | Memory: 146.6 MB (6.17%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -16,19 +16,22 @@ class Solution {
         int totalCost, currCost, i, j, nextCost, newi, newj;
         while(!pq.isEmpty()){
             tmp = pq.poll();
-            totalCost = Math.abs(tmp[0]); currCost = tmp[0] < 0 ? 2 : 1;
+            totalCost = tmp[0] < 0 ? tmp[0]*-1 : tmp[0]; currCost = tmp[0] < 0 ? 2 : 1;
             i = tmp[1]; j = tmp[2];
             nextCost = currCost == 1 ? 2 : 1;
 
             if(i == m-1 && j == n-1) return totalCost;
             if(vis[i][j] != -1 && vis[i][j] <= totalCost) continue;
             vis[i][j] = totalCost;
-
+            totalCost += currCost;
             for(int k=0; k<4; k++){
                 newi = dr[k] + i; newj = dc[k] + j;
                 if(newi < m && newi >= 0 && newj < n && newj >= 0){
                     tmp = new Integer[3];
-                    tmp[0] = Math.max(moveTime[newi][newj] + currCost, totalCost + currCost); 
+                    if((moveTime[newi][newj] + currCost) > totalCost){
+                        tmp[0] = (moveTime[newi][newj] + currCost);
+                    }
+                    else tmp[0] = totalCost;
                     tmp[1] = newi; tmp[2] = newj;
                     if(nextCost == 2) tmp[0] *= -1;
                     pq.add( tmp );
