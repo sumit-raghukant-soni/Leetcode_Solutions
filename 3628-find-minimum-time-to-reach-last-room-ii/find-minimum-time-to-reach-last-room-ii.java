0bc2493a9548eed584a3d5430f49ff7cb847b01f
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-05-08 22:00:12 +0530

    Time: 1380 ms (5.29%) | Memory: 146.9 MB (5.73%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -3,20 +3,25 @@ class Solution {
         int m = moveTime.length, n = moveTime[0].length;
         int dr[] = {0, 1, 0, -1}, dc[] = {1, 0, -1, 0};
         int vis[][] = new int[m][n];
-        PriorityQueue< Integer[] > pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue< Integer[] > pq = new PriorityQueue<>((a, b) -> {
+            if(a[0] == b[0]){
+                return (b[1]+b[2]) - (a[1]+a[2]);
+            }
+            return a[0] - b[0];
+        });
 
         for(int i=0; i<m; i++){
             Arrays.fill(vis[i], -1);
         }
 
-        Integer[] tmp = new Integer[4];
-        tmp[0] = 0; tmp[1] = 1; tmp[2] = 0; tmp[3] = 0;
+        Integer[] tmp = new Integer[3];
+        tmp[0] = 0; tmp[1] = 0; tmp[2] = 0;
         pq.add( tmp );
 
         while(!pq.isEmpty()){
             tmp = pq.poll();
-            int totalCost = tmp[0], currCost = tmp[1];
-            int i = tmp[2], j = tmp[3];
+            int totalCost = Math.abs(tmp[0]), currCost = tmp[0] < 0 ? 2 : 1;
+            int i = tmp[1], j = tmp[2];
             int nextCost = currCost == 1 ? 2 : 1;
 
             if(i == m-1 && j == n-1) return totalCost;
@@ -26,9 +31,10 @@ class Solution {
             for(int k=0; k<4; k++){
                 int newi = dr[k] + i, newj = dc[k] + j;
                 if(newi < m && newi >= 0 && newj < n && newj >= 0){
-                    tmp = new Integer[4];
+                    tmp = new Integer[3];
                     tmp[0] = Math.max(moveTime[newi][newj] + currCost, totalCost + currCost); 
-                    tmp[1]= nextCost; tmp[2] = newi; tmp[3] = newj;
+                    tmp[1] = newi; tmp[2] = newj;
+                    if(nextCost == 2) tmp[0] *= -1;
                     pq.add( tmp );
                 }
             }
