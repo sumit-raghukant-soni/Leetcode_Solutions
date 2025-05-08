50aca881dd8784ca1f57d92366612f3d200b7ea3
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-05-08 21:52:15 +0530

    Time: 1014 ms (5.29%) | Memory: 126.8 MB (10.57%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -3,21 +3,21 @@ class Solution {
         int m = moveTime.length, n = moveTime[0].length;
         int dr[] = {0, -1, 0, 1}, dc[] = {-1, 0, 1, 0};
         int vis[][] = new int[m][n];
-        PriorityQueue< Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> > pq = new PriorityQueue<>((a, b) -> a.getKey().getKey() - b.getKey().getKey());
+        PriorityQueue< Integer[] > pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
         for(int i=0; i<m; i++){
             Arrays.fill(vis[i], -1);
         }
 
-        pq.add( new Pair(new Pair(0, 1), new Pair(0, 0)) );
+        Integer[] tmp = new Integer[4];
+        tmp[0] = 0; tmp[1] = 1; tmp[2] = 0; tmp[3] = 0;
+        pq.add( tmp );
 
         while(!pq.isEmpty()){
-            int totalCost = pq.peek().getKey().getKey(), currCost = pq.peek().getKey().getValue();
-            int i = pq.peek().getValue().getKey(), j = pq.peek().getValue().getValue();
+            tmp = pq.poll();
+            int totalCost = tmp[0], currCost = tmp[1];
+            int i = tmp[2], j = tmp[3];
             int nextCost = currCost == 1 ? 2 : 1;
-            pq.poll();
-
-            // if(m == 2 && n == 3) System.out.println(i + " " + j + " = " + totalCost);
 
             if(i == m-1 && j == n-1) return totalCost;
             if(vis[i][j] != -1 && vis[i][j] <= totalCost) continue;
@@ -26,7 +26,10 @@ class Solution {
             for(int k=0; k<4; k++){
                 int newi = dr[k] + i, newj = dc[k] + j;
                 if(newi < m && newi >= 0 && newj < n && newj >= 0){
-                    pq.add( new Pair( new Pair(Math.max(moveTime[newi][newj] + currCost, totalCost + currCost) , nextCost), new Pair(newi, newj) ) );
+                    tmp = new Integer[4];
+                    tmp[0] = Math.max(moveTime[newi][newj] + currCost, totalCost + currCost); 
+                    tmp[1]= nextCost; tmp[2] = newi; tmp[3] = newj;
+                    pq.add( tmp );
                 }
             }
         }
