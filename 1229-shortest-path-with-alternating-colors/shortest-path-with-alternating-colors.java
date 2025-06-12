0d5d12084c396db66e8e3ef31d688d30d32966a2
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-06-12 22:06:30 +0530

    Time: 61 ms (5.17%) | Memory: 45.5 MB (10.99%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.java
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.java
@@ -21,8 +21,9 @@ class Solution {
 
     public int solve(int[][] adj1, int[][] adj2, int n, int dest){
         int cnt = 0, vis[][] = new int[n][3];
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        q.add(new Pair(0, 0));
+        Queue<Integer[]> q = new LinkedList<>();
+        Integer t[] = {0, 0};
+        q.add(t);
         q.add(null);
         vis[0][0] = 1;
 
@@ -33,18 +34,20 @@ class Solution {
                 q.add(null);
                 cnt++;
             }
-            int curr = q.peek().getKey(), c = q.peek().getValue();
+            int curr = q.peek()[0], c = q.peek()[1];
             q.poll();
             if(curr == dest) return cnt;
             
             for(int i=0; i<n; i++){
                 if(adj1[curr][i] != 0 && adj1[curr][i] != c && vis[i][adj1[curr][i]] == 0){
                     vis[i][adj1[curr][i]] = 1;
-                    q.add(new Pair(i, adj1[curr][i]));
+                    Integer[] tmp = {i, adj1[curr][i]};
+                    q.add(tmp);
                 }
                 if(adj2[curr][i] != 0 && adj2[curr][i] != c && vis[i][adj2[curr][i]] == 0){
                     vis[i][adj2[curr][i]] = 1;
-                    q.add(new Pair(i, adj2[curr][i]));
+                    Integer[] tmp = {i, adj2[curr][i]};
+                    q.add(tmp);
                 }
             }
         }
