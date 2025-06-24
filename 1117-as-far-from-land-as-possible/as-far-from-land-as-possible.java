375bae15e1e162c4d9a788c61beb386e8f0e7caa
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-06-24 23:31:03 +0530

    Time: 28 ms (6.06%) | Memory: 45.9 MB (36.36%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -2,16 +2,16 @@ class Solution {
     public int maxDistance(int[][] grid) {
         int n = grid.length, ans = -1;
         int vis[][] = new int[n][n], dr[] = {0, 1, 0, -1}, dc[] = {1, 0, -1, 0};
-        List<Integer> tmp = null;
-        Queue<List<Integer>> q = new LinkedList<>();
+        Integer[] tmp = null;
+        Queue<Integer[]> q = new LinkedList<>();
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 1){
-                    tmp = new ArrayList<>();
-                    tmp.add(i);
-                    tmp.add(j);
-                    tmp.add(0);
+                    tmp = new Integer[3];
+                    tmp[0] = i;
+                    tmp[1] = j;
+                    tmp[2] = 0;
                     q.add(tmp);
                 }
             }
@@ -19,19 +19,19 @@ class Solution {
 
         while(!q.isEmpty()){
             tmp = q.poll();
-            int i = tmp.get(0), j = tmp.get(1), dis = tmp.get(2);
+            int i = tmp[0], j = tmp[1], dis = tmp[2];
             if(vis[i][j] != 0) continue;
             // System.out.println(i + ":" + j + " = " + dis);
-            ans = Math.max(ans, dis);
+            ans = ans > dis ? ans : dis;
             vis[i][j] = dis;
             
             for(int k=0; k<4; k++){
                 int newi = i + dr[k], newj = j + dc[k];
                 if(newi < n && newj < n && newi >= 0 && newj >= 0 && grid[newi][newj] == 0 && vis[newi][newj] == 0){
-                    tmp = new ArrayList<>();
-                    tmp.add(newi);
-                    tmp.add(newj);
-                    tmp.add(dis+1);
+                    tmp = new Integer[3];
+                    tmp[0] = newi;
+                    tmp[1] = newj;
+                    tmp[2] = dis+1;
                     q.add(tmp);
                 }
             }
