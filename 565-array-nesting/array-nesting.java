f7f99100fbeb6146d0253cf4e903f161efc98afd
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-07-03 22:04:12 +0530

    Time: 13 ms (19.67%) | Memory: 66.1 MB (6.83%) - LeetSync

---
+++ 565-array-nesting/array-nesting.java
@@ -0,0 +1,30 @@
+class Solution {
+    int vis[] = null, ans = 0;
+    public int arrayNesting(int[] nums) {
+        int sz = nums.length;
+        vis = new int[sz];
+        Arrays.fill(vis, -1);
+
+        for(int i=0; i<sz; i++){
+            if(vis[i] == -1){
+                solve(nums, i);
+            }
+            // System.out.print(vis[i] + ",");
+            ans = ans > vis[i] ? ans : vis[i];
+        }
+
+        return ans;
+    }
+
+    private int solve(int[] nums, int i){
+        
+        int cnt = 0;
+        vis[i] = 0;
+        if(vis[nums[i]] == -1){
+            cnt += solve(nums, nums[i]);
+        }
+        else cnt += vis[nums[i]];
+
+        return vis[i] = cnt+1;
+    }
+}
