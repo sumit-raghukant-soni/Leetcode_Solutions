e87d6ad25559b692915a87d06b753de984add030
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-07-29 22:14:14 +0530

    Time: 8 ms (80.39%) | Memory: 41.1 MB (64.01%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,35 +1,23 @@
 class Solution {
-    public void solve(int[] nums, int sz, HashMap<Integer, Integer> mp, int tmp, int ind){
-        if(ind == sz){
-            if(mp.containsKey(tmp)){
-                mp.put(tmp, mp.get(tmp)+1);
-            }
-            else{
-                mp.put(tmp, 1);
-            }
-            return;
-        }
-        
-        solve(nums, sz, mp, tmp, ind+1);
-        tmp = tmp | nums[ind];
-        // System.out.print(i + " = " + tmp + " ");
-        solve(nums, sz, mp, tmp, ind+1);
-
-    }
+    public int ans = 0, maxi = 0;
     public int countMaxOrSubsets(int[] nums) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        Integer maxi = 0, cnt = 0, sz = nums.length;
-        
-        solve(nums, sz, mp, 0, 0);
+        int sz = nums.length;
 
-        for(Map.Entry<Integer, Integer> e : mp.entrySet()){
-            // System.out.println(e.getKey() + " " + e.getValue());
-            if(e.getKey() > maxi){
-                maxi = e.getKey();
-                cnt = e.getValue();
-            }
+        for(int i=0; i<sz; i++){
+            maxi = maxi | nums[i];
         }
+        
+        solve(nums, sz, 0, 0);
 
-        return cnt;
+        return ans;
+    }
+
+    private void solve(int[] nums, int sz, int i, int sum){
+        if(i == sz) return;
+
+        solve(nums, sz, i+1, sum);
+        sum = sum|nums[i];
+        if(sum == maxi) ans++;
+        solve(nums, sz, i+1, sum);
     }
 }
