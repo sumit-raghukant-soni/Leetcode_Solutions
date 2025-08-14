54ff966b7e064480b10380847f1264eaa202de1b
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-08-15 04:52:25 +0530

    Time: 113 ms (37.61%) | Memory: 46.6 MB (81.89%) - LeetSync

--- 753-open-the-lock/open-the-lock.java
+++ 753-open-the-lock/open-the-lock.java
@@ -12,61 +12,27 @@ class Solution {
         vis.add("0000");
         while(!q.isEmpty()){
             String curr = q.peek().getKey();
+            StringBuilder str = new StringBuilder(curr);
             int cnt = q.poll().getValue();
             if(curr.equals(target)) return cnt;
             
             // System.out.println(curr);
-            int f = curr.charAt(0) - '0',s = curr.charAt(1) - '0',t = curr.charAt(2) - '0',fr = curr.charAt(3) - '0';
-            String tmp = (char) ((f < 9 ? f+1 : 0) + '0') + curr.substring(1);
-            // System.out.println("1st " + tmp);
-            if(!vis.contains(tmp) && !st.contains(tmp)){
-                q.add(new Pair(tmp, cnt+1));
-                vis.add(tmp);
-            }
-            tmp = (char) ((f > 0 ? f-1 : 9) + '0') + curr.substring(1);
-            // System.out.println("2nd " + tmp);
-            if(!vis.contains(tmp) && !st.contains(tmp)){
-                q.add(new Pair(tmp, cnt+1));
-                vis.add(tmp);
-            }
-
-            tmp = curr.substring(0, 1) + ((char) ((s < 9 ? s+1 : 0) + '0')) + curr.substring(2);
-            // System.out.println("3rd " + tmp);
-            if(!vis.contains(tmp) && !st.contains(tmp)){
-                q.add(new Pair(tmp, cnt+1));
-                vis.add(tmp);
-            }
-            tmp = curr.substring(0, 1) + ((char) ((s > 0 ? s-1 : 9) + '0')) + curr.substring(2);
-            // System.out.println("4th " + tmp);
-            if(!vis.contains(tmp) && !st.contains(tmp)){
-                q.add(new Pair(tmp, cnt+1));
-                vis.add(tmp);
-            }
-
-            tmp = curr.substring(0,2) + (char) ((t < 9 ? t+1 : 0) + '0') + curr.substring(3, 4);
-            // System.out.println("5th " + tmp);
-            if(!vis.contains(tmp) && !st.contains(tmp)){
-                q.add(new Pair(tmp, cnt+1));
-                vis.add(tmp);
-            }
-            tmp = curr.substring(0,2) + (char) ((t > 0 ? t-1 : 9) + '0') + curr.substring(3, 4);
-            // System.out.println("6th " + tmp);
-            if(!vis.contains(tmp) && !st.contains(tmp)){
-                q.add(new Pair(tmp, cnt+1));
-                vis.add(tmp);
-            }
-
-            tmp = curr.substring(0,3) + (char) ((fr < 9 ? fr+1 : 0) + '0');
-            // System.out.println("7th " + tmp);
-            if(!vis.contains(tmp) && !st.contains(tmp)){
-                q.add(new Pair(tmp, cnt+1));
-                vis.add(tmp);
-            }
-            tmp = curr.substring(0,3) + (char) ((fr > 0 ? fr-1 : 9) + '0');
-            // System.out.println("8th " + tmp);
-            if(!vis.contains(tmp) && !st.contains(tmp)){
-                q.add(new Pair(tmp, cnt+1));
-                vis.add(tmp);
+            for(int i=0; i<4; i++){
+                int f = curr.charAt(i) - '0';
+                str.setCharAt(i, (char) ((f < 9 ? f+1 : 0) + '0'));
+                String tmp = str.toString();
+                str.setCharAt(i, curr.charAt(i));
+                if(!vis.contains(tmp) && !st.contains(tmp)){
+                    q.add(new Pair(tmp, cnt+1));
+                    vis.add(tmp);
+                }
+                str.setCharAt(i, (char) ((f > 0 ? f-1 : 9) + '0'));
+                tmp = str.toString();
+                str.setCharAt(i, curr.charAt(i));
+                if(!vis.contains(tmp) && !st.contains(tmp)){
+                    q.add(new Pair(tmp, cnt+1));
+                    vis.add(tmp);
+                }
             }
         }
 
