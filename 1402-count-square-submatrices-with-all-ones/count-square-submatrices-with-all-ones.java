806e16a3e6fda5f21d00bbfa0452a111ab470fa2
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-08-20 21:33:01 +0530

    Time: 8 ms (18.33%) | Memory: 55.2 MB (53.73%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,36 +1,18 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-        int row = matrix.length, col = matrix[0].length, ans = 0;
-        int dp[][] = new int[row][col];
+        int m = matrix.length, n = matrix[0].length, ans = 0;
+        int dp[][] = new int[m][n];
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (matrix[i][j] == 1) {
-                    int top = (i - 1 >= 0 ? dp[i - 1][j] : 0), left = (j - 1 >= 0 ? dp[i][j - 1] : 0),
-                            diag = (i - 1 >= 0 && j - 1 >= 0 ? dp[i - 1][j - 1] : 0);
-                    dp[i][j] = Math.min(top, Math.min(left, diag));
-                    dp[i][j]++;
-                }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0) continue;
+                int left = j-1 >= 0 ? dp[i][j-1] : 0, 
+                up = i-1 >= 0 ? dp[i-1][j] : 0, 
+                diag = (j-1 >= 0 && i-1 >= 0) ? dp[i-1][j-1] : 0;
+                int cnt = Math.min(Math.min(diag, up), left);
+                dp[i][j] = cnt + 1;
+                ans += cnt + 1;
             }
-        }
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (dp[i][j] != 0) {
-                    if (i - 1 >= 0
-                            && j - 1 >= 0
-                            && dp[i - 1][j] > 0
-                            && dp[i][j - 1] > 0
-                            && dp[i - 1][j - 1] > 0) {
-                        // System.out.println("this " + i + " " + j);
-                        ans += Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
-                    }
-                    ans++;
-                }
-
-                // System.out.print(dp[i][j] + " ");
-            }
-            // System.out.println();
         }
 
         return ans;
