5d4d728c7f623bad7cef8501561811e014b4778d
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-09-03 21:46:51 +0530

    Time: 55 ms (6.42%) | Memory: 145.6 MB (80.73%) - LeetSync

--- 3388-right-triangles/right-triangles.java
+++ 3388-right-triangles/right-triangles.java
@@ -4,35 +4,23 @@ class Solution {
         long ans = 0;
         int up[][] = new int[m][n], down[][] = new int[m][n], left[][] = new int[m][n], right[][] = new int[m][n];
 
-        for(int i=0; i<m; i++){
-            int cnt = 0;
-            for(int j=0; j<n; j++){
-                left[i][j] = cnt;
-                if(grid[i][j] == 1) cnt++;
+        for(int i1=0; i1<m; i1++){
+            int cnt1 = 0, cnt2 = 0;
+            for(int j1=0, j2 = n-1; j1<n; j1++, j2--){
+                left[i1][j1] = cnt1;
+                cnt1 += grid[i1][j1] == 1 ? 1 : 0;
+                right[i1][j2] = cnt2;
+                cnt2 += grid[i1][j2] == 1 ? 1 : 0;
             }
         }
 
-        for(int i=0; i<m; i++){
-            int cnt = 0;
-            for(int j=n-1; j>=0; j--){
-                right[i][j] = cnt;
-                if(grid[i][j] == 1) cnt++;
-            }
-        }
-
-        for(int i=0; i<n; i++){
-            int cnt = 0;
-            for(int j=m-1; j>=0; j--){
-                down[j][i] = cnt;
-                if(grid[j][i] == 1) cnt++;
-            }
-        }
-
-        for(int i=0; i<n; i++){
-            int cnt = 0;
-            for(int j=0; j<m; j++){
-                up[j][i] = cnt;
-                if(grid[j][i] == 1) cnt++;
+        for(int i1=0; i1<n; i1++){
+            int cnt1 = 0, cnt2 = 0;
+            for(int j1=0, j2 = m-1; j1<m; j1++, j2--){
+                up[j1][i1] = cnt1;
+                cnt1 += grid[j1][i1] == 1 ? 1 : 0;
+                down[j2][i1] = cnt2;
+                cnt2 += grid[j2][i1] == 1 ? 1 : 0;
             }
         }
 
