40f0394c5a3eff6c5664510d1579d98a16ae7121
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-09-12 22:37:36 +0530

    Time: 269 ms (5.03%) | Memory: 67.4 MB (5.01%) - LeetSync

---
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        int sz1 = languages.length, sz2 = friendships.length, ans = Integer.MAX_VALUE, sz3 = 0;
+        Map<Integer, List<Integer>> mp = new HashMap<>();
+        Set<Integer> lst = new HashSet<>();
+        Set<Integer> languagesKnown = new HashSet<>();
+
+        for(int i=0; i<sz1; i++){
+            List<Integer> tmp = new ArrayList<>();
+            
+            for(int j : languages[i]){
+                languagesKnown.add(j);
+                tmp.add(j);
+            } 
+
+            mp.put(i+1, tmp);
+        }
+
+        for(int i=0; i<sz2; i++){
+            boolean flg = false;
+            for(int j : mp.get(friendships[i][0])){
+                if(mp.get(friendships[i][1]).contains(j)){
+                    flg = true;
+                    break;
+                }
+            }
+            if(!flg){
+                // System.out.println("from " + mp.get(friendships[i][0]));
+                // System.out.println("to " + mp.get(friendships[i][1]));
+                // System.out.println(friendships[i][0] + ":" + friendships[i][1]);
+                lst.add(friendships[i][0]);
+                lst.add(friendships[i][1]);
+            }
+        }
+
+        if(lst.isEmpty()) return 0;
+        
+        System.out.println(lst);
+        System.out.println(mp);
+        sz3 = lst.size();
+        for(int i : languagesKnown){
+            int cnt = 0;
+            for(int j : lst){
+                if(mp.get(j).contains(i)) cnt++;
+            }
+            System.out.println(i + " " + cnt);
+            ans = ans < (sz3 - cnt) ? ans : (sz3 - cnt);
+        }
+        
+        return ans;
+    }
+}
