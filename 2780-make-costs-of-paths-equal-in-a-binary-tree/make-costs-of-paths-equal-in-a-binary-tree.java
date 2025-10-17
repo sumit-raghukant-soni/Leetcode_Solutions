b583a052938a568b55f25b01b4f64e34532f617e
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-10-18 01:37:08 +0530

    Time: 4 ms (99.34%) | Memory: 55.1 MB (89.40%) - LeetSync

--- 2780-make-costs-of-paths-equal-in-a-binary-tree/make-costs-of-paths-equal-in-a-binary-tree.java
+++ 2780-make-costs-of-paths-equal-in-a-binary-tree/make-costs-of-paths-equal-in-a-binary-tree.java
@@ -1,46 +1,13 @@
 class Solution {
-    Map<Integer, List<Integer>> mp = new HashMap<>();
-    int ans = 0;
     public int minIncrements(int n, int[] cost) {
-        int i=0;
-        Tree root = new Tree(cost[i], i);
-        Queue<Tree> q = new LinkedList<>();
-        q.add(root);
+        int ans = 0;
 
-        while(i < n-1){
-            // System.out.println(i + " " + n);
-            Tree curr = q.poll();
-            Tree left = new Tree(cost[++i], i);
-            Tree right = new Tree(cost[++i], i);
-            // System.out.println(curr.ind + " " + left.ind + ":" + right.ind);
-            curr.left = left;
-            curr.right = right;
-            q.add(left);
-            q.add(right);
+        for(int i=n-1; i>0; i-=2){
+            int a = cost[i], b = cost[i-1];
+            ans += a > b ? (a-b) : (b-a);
+            cost[(i-1)/2] += a > b ? a : b;
         }
 
-        solve(root);
-
         return ans;
-    }
-
-    private int solve(Tree root){
-        if(root == null) return 0;
-        // System.out.println("Inside " + root.ind);
-        int sum = 0;
-        int left = solve(root.left), right = solve(root.right);
-        ans += Math.abs(left - right);
-        // System.out.println(root.ind);
-        // System.out.println(left + ":" + right);
-        return Math.max(left, right) + root.val;
-    }
-}
-
-class Tree{
-    int val, ind;
-    Tree left, right;
-    Tree(int v, int i){
-        this.val = v;
-        this.ind = i;
     }
 }
