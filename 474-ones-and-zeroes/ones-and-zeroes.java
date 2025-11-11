eb66110b622bbb68640b466efeb51bc17d2f8049
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-11-11 23:17:01 +0530

    Time: 247 ms (8.71%) | Memory: 181.9 MB (5.01%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.java
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -1,17 +1,10 @@
 class Solution {
     int ones[] = null, zeros[] = null;
-    int dp[][][] = null;
+    Map<Integer, Integer> mp = new HashMap<>();
     public int findMaxForm(String[] strs, int m, int n) {
         int sz = strs.length;
-        // PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>();
         ones = new int[sz];
         zeros = new int[sz];
-        dp = new int[sz][101][101];
-        for(int i=0; i<sz; i++){
-            for(int j=0; j<101; j++){
-                Arrays.fill(dp[i][j], -1);
-            }
-        }
 
         for(int i=0; i<sz; i++){
             int one = 0, zero = 0;
@@ -30,13 +23,15 @@ class Solution {
 
     private int solve(int i, int sz, int m, int n){
         if(i >= sz) return 0;
-        if(dp[i][m][n] != -1) return dp[i][m][n];
+        int key = (i * 100000000) + (m * 100000) + n;
+        if(mp.containsKey(key)) return mp.get(key);
 
         int take = 0, notTake = solve(i+1, sz, m, n);
         if(n >= ones[i] && m >= zeros[i]){
             take = 1 + solve(i+1, sz, m - zeros[i], n - ones[i]);
         }
 
-        return dp[i][m][n] = take > notTake ? take : notTake;
+        mp.put(key, take > notTake ? take : notTake);
+        return take > notTake ? take : notTake;
     }
 }
