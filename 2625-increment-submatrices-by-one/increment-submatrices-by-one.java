17d77d5aad8732fcd0ec7686c5f3bed3db65e712
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-11-15 00:43:24 +0530

    Time: 2011 ms (5.26%) | Memory: 93.6 MB (6.14%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int sz = queries.length;
+        int arr[][] = new int[n][n], flg[][] = new int[n][n];
+
+        for(int i=0; i<sz; i++){
+            int r1 = queries[i][0], r2 = queries[i][2];
+            int c1 = queries[i][1], c2 = queries[i][3];
+            flg[r1][c1] += 1;
+            if(r2+1 < n && c2+1 < n) flg[r2+1][c2+1] += 1;
+            if(r2+1 < n) flg[r2+1][c1] -= 1;
+            if(c2+1 < n) flg[r1][c2+1] -= 1;
+        }
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         System.out.print(flg[i][j] + ",");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int i=0; i<n; i++){
+            int cnt = 0;
+            for(int j=0; j<n; j++){
+                if(i+1 < n) flg[i+1][j] += flg[i][j];
+                cnt += flg[i][j];
+                System.out.println(i + ":" + j + " = " + cnt);
+                arr[i][j] = cnt;
+            }
+        }
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         System.out.print(flg[i][j] + ",");
+        //     }
+        //     System.out.println();
+        // }
+
+        return arr;
+    }
+}
