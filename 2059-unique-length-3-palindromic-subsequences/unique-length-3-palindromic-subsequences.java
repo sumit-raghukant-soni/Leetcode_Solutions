8122d585f74b74703b83101760e36bf7b6886bc1
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2025-11-22 19:39:19 +0530

    Time: 31 ms (98.46%) | Memory: 46.9 MB (24.31%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,42 +1,48 @@
 class Solution {
-    Set<String> st;
-    Set<Character> st2;
     public int countPalindromicSubsequence(String s) {
-        // st = new HashSet<>();
-        st2 = new HashSet<>();
+        int sz = s.length(), cnt = 0;
+        int map[][] = new int[26][26]; 
+        int pre[] = new int[26], next[] = new int[26];
+        
+        for(int i=1; i<sz; i++){
+            next[s.charAt(i) - 'a']++;
+        }
 
-        // solve(s, "", "", 0);
-
-        int alpha[] = new int[26], ans = 0;
-        for(int i=0; i<s.length(); i++){
-            if(!st2.contains(s.charAt(i))){
-                int j = s.lastIndexOf(s.charAt(i));
-                if(i != j){
-                    st2.add(s.charAt(i));
-                    Arrays.fill(alpha, 0);
-                    for(int k=i+1; k<j; k++){
-                        alpha[s.charAt(k) - 'a']++;
-                    }
-                    for(int k=0; k<26; k++){
-                        if(alpha[k] != 0) ans++;
-                    }
-                }
+        pre[s.charAt(0) - 'a']++;
+        for(int i=1; i<sz-1; i++){
+            char curr = s.charAt(i);
+            // System.out.print(curr);
+            next[curr - 'a']--;
+            for(int j=0; j<26; j++){
+                if(pre[j] == 0 || next[j] == 0 || map[curr-'a'][j] > 0) continue;
+                // System.out.println(curr + " " + (char) (j + 'a'));
+                map[curr-'a'][j]++;
+                cnt++;
             }
+            pre[curr - 'a']++;
+            // i++;
+            // while(i < sz && s.charAt(i) == curr){
+            //     curr = s.charAt(i);
+            //     next[curr - 'a']--;
+            //     pre[curr - 'a']++;
+            //     i++;
+            // }
+            // i--;
+            // for(int j=0; j<26; j++) System.out.print((char) (j+'a') + " ");
+            // System.out.println();
+            // for(int j=0; j<26; j++) System.out.print(pre[j]);
+            // System.out.println();
+            // for(int j=0; j<26; j++) System.out.print(next[j]);
+            // System.out.println();
         }
-        return ans;
 
-        // return st.size();
-    }
+        // for(int j=0; j<26; j++){
+        //     for(int i=0; i<26; i++){
+        //         System.out.print(map[j][i] + ",");
+        //     }
+        //     System.out.println();
+        // }
 
-    public void solve(String s, String str, String rev, int i){
-        if(str.length() == 3){
-            // System.out.println(str + " " + rev);
-            if(str.equals(rev)) st.add(str);
-            return;
-        }
-        if(i == s.length()) return;
-
-        solve(s, str+s.charAt(i), s.charAt(i) + rev, i+1);
-        solve(s, str, rev, i+1);
+        return cnt;
     }
 }
