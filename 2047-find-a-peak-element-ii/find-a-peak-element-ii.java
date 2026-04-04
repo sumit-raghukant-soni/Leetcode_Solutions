aae63294effedc230949d5360835c9972375cb4c
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2026-04-04 17:25:16 +0530

    Time: 0 ms (100.00%) | Memory: 116.3 MB (52.45%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,20 +1,32 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int m = mat.length, n = mat[0].length, cnt;
-        int dr[] = {0, 1, 0, -1}, dc[] = {1, 0, -1, 0};
+        int m = mat.length, n = mat[0].length;
+        
+        // if(n > m){
+            return solveColWise(mat, m, n);
+        // }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                cnt = 0;
-                for(int k=0; k<4; k++){
-                    int r = dr[k] + i, c = dc[k] + j;
-                    if(r >= 0 && r < m && c >= 0 && c < n){
-                        if(mat[r][c] < mat[i][j]) cnt++;
-                    }
-                    else cnt++;
-                }
-                if(cnt == 4) return new int[]{i, j};
+        // return new int[]{0,0};
+    }
+
+    private int[] solveColWise(int[][] mat, int m, int n) {
+        int s = 0, e = n-1;
+
+        while(s <= e){
+            int currRow = 0;
+            int mid = s + (e-s)/2;
+            for(int i=0; i<m; i++){
+                currRow = mat[currRow][mid] >= mat[i][mid] ? currRow : i;
             }
+            boolean isLeftBigger = mid-1 >= 0 ? (mat[currRow][mid-1] > mat[currRow][mid]) : false;
+            boolean isRightBigger = mid+1 < n ? (mat[currRow][mid+1] > mat[currRow][mid]) : false;
+            if(!isLeftBigger && !isRightBigger){
+                return new int[]{currRow, mid};
+            }
+            else if(isLeftBigger){
+                e = mid - 1;
+            }
+            else s = mid + 1;
         }
 
         return new int[]{0,0};
