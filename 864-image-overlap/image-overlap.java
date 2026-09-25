46f5b62e0c87cf64b5419b2bbb204843261819a3
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2026-09-25 22:39:11 +0530

    Time: 43 ms (53.65%) | Memory: 44.3 MB (93.60%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -1,28 +1,56 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
-        int n=img1.length;
-        List<Pair<Integer,Integer>>vp1 = new ArrayList<>(),vp2 = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(img1[i][j]==1){
-                    vp1.add(new Pair(i, j));
-                }
-                if(img2[i][j]==1){
-                    vp2.add(new Pair(i, j));
+        int sz = img1.length;
+        int ans = 0;
+        int r1[] = new int[sz], c1[] = new int[sz];
+        int r2[] = new int[sz], c2[] = new int[sz];
+
+        for(int i=0; i<sz; i++) {
+            for(int j=0; j<sz; j++) {
+                if(img1[i][j] == 1) {
+                    r1[i]++; c1[j]++;
                 }
             }
         }
-        int ans=0;
-        Map<Pair<Integer,Integer>,Integer>mp = new HashMap<>();
-        for(Pair<Integer, Integer> it1:vp1){
-            for(Pair<Integer, Integer> it2:vp2){
-                int a=it1.getKey()-it2.getKey();
-                int b=it1.getValue()-it2.getValue();
-                Pair<Integer, Integer> p = new Pair(a, b);
-                mp.put(p, mp.getOrDefault(p, 0) + 1);
-                ans = Math.max(ans,mp.get(p));
+
+        for(int i=0; i<sz; i++) {
+            for(int j=0; j<sz; j++) {
+                if(img2[i][j] == 1) {
+                    r2[i]++; c2[j]++;
+                }
             }
         }
+
+        for(int k=0; k<sz; k++) {
+            for(int m=0; m<sz; m++) {
+                int tmp1 = 0, tmp2 = 0, tmp3 = 0, tmp4 = 0, newi, newj;
+                for(int i=0; i<sz; i++) {
+                    for(int j=0; j<sz; j++) {
+                        newi = i + k; newj = j + m;
+                        if(newi < sz && newj < sz && img1[i][j] + img2[newi][newj] == 2) {
+                            tmp1++;
+                        }
+                        newi = i - k; newj = j - m;
+                        if(newi >= 0 && newj >= 0 && img1[i][j] + img2[newi][newj] == 2) {
+                            tmp2++;
+                        }
+                        newi = i + k; newj = j - m;
+                        if(newi < sz && newj >= 0 && img1[i][j] + img2[newi][newj] == 2) {
+                            tmp3++;
+                        }
+                        newi = i - k; newj = j + m;
+                        if(newi >= 0 && newj < sz && img1[i][j] + img2[newi][newj] == 2) {
+                            tmp4++;
+                        }
+                    }
+                }
+                ans = ans > tmp1 ? ans : tmp1;
+                ans = ans > tmp2 ? ans : tmp2;
+                ans = ans > tmp3 ? ans : tmp3;
+                ans = ans > tmp4 ? ans : tmp4;
+            }
+        }
+
         return ans;
     }
 }
